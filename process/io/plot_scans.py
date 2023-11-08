ba6077c0d3cf548a12bb3dfa41c481ff0cb5d18e
Chris <christopher.ashe@ukaea.uk>
2023-11-08 14:09:56 +0000

    first metadata call addition

--- process/io/plot_scans.py
+++ process/io/plot_scans.py
@@ -887,8 +887,8 @@ def main(args=None):
                     plt.plot(scan_2_var_array, output_array, "--o", label=labl)
 
                 plt.grid(True)
-                plt.ylabel(labels[output_name], fontsize=axis_font_size)
-                plt.xlabel(labels[scan_2_var_name], fontsize=axis_font_size)
+                plt.ylabel(metadata[output_name].labels, fontsize=axis_font_size)
+                plt.xlabel(metadata[scan_2_var_name].labels, fontsize=axis_font_size)
                 plt.legend(loc="best", fontsize=legend_size)
                 plt.xticks(size=axis_tick_size)
                 plt.yticks(size=axis_tick_size)
