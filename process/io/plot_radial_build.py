b3f0e7e3f2c2b4680746259e236c4ce79639d3e5
mn3981 <christopher.ashe@ukaea.uk>
2023-12-07 10:55:55 +0000

    black fixes

--- process/io/plot_radial_build.py
+++ process/io/plot_radial_build.py
@@ -264,7 +264,7 @@ def main(args=None):
         "tbrmin",
         "bt",
         "coreradius",
-        "Obsolete"  # Removed
+        "Obsolete",  # Removed
         "taulimit",
         "epsvmc",
         "ttarget",
