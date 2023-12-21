1797e0c67d0c4fd3f145a434bd9c9d6aa8df9309
Clair Mould <86794332+clmould@users.noreply.github.com>
2023-12-21 16:09:57 +0000

    stop opt.dat being created and remove references to it in docs

--- source/fortran/init_module.f90
+++ source/fortran/init_module.f90
@@ -150,7 +150,6 @@ contains
       open(unit=nout     ,file=trim(output_prefix)//'OUT.DAT'   ,status='unknown')
       open(unit=nplot    ,file=trim(output_prefix)//'PLOT.DAT'  ,status='unknown')
       open(unit=mfile    ,file=trim(output_prefix)//'MFILE.DAT' ,status='unknown')
-      open(unit=opt_file ,file=trim(output_prefix)//'OPT.DAT'   ,status='unknown')
       open(unit=sig_file ,file=trim(output_prefix)//'SIG_TF.DAT',status='unknown')
 
       !  Input any desired new initial values
