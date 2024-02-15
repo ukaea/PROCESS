00e17006826ec95cf24f0fb4db2880d48c0bc573
Graeme <107113942+ym1906@users.noreply.github.com>
2024-02-15 15:25:48 +0000

    Added check to see if fimp(2) deviates from default value, if it does… (#3053)
    * Added check to see if fimp(2) deviates from default value, if it does write a warning message to use ralpne instead.
    
    * Added stop to enforce the message, mofidified message

--- source/fortran/initial.f90
+++ source/fortran/initial.f90
@@ -347,6 +347,12 @@ subroutine check
         trithtmw = 0.0D0
     end if
 
+    if (fimp(2) .ne. 0.1D0) then
+        write(*,*)'The thermal alpha/electron density ratio should be controlled using ralpne (itv 109) and not fimp(2).'
+        write(*,*)'fimp(2) should be removed from the input file, or set to the default value 0.1D0.'
+        stop 1
+    end if
+
     !  Impurity fractions
     do imp = 1,nimp
         impurity_arr_frac(imp) = fimp(imp)
