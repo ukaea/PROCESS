705df3783948556b6f150b484786d9c61eb4aed5
Timothy <75321887+timothy-nunn@users.noreply.github.com>
2024-03-21 14:31:54 +0000

    Upversion process to 3.1.0 (#3111)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if platform.system() == "Darwin":
 
 setup_kwargs = {
     "name": MODULE_NAME,
-    "version": "3.0.2",
+    "version": "3.1.0",
     "description": (
         "Power Reactor Optimisation Code for Environmental and Safety Studies"
     ),
--- source/fortran/main_module.f90
+++ source/fortran/main_module.f90
@@ -48,7 +48,7 @@ subroutine inform(progid)
   character(len=10) :: progname
   character(len=98) :: executable
   character(len=*), parameter :: progver = &  !  Beware: keep exactly same format...
-       '3.0.2   Release Date :: 2024-01-25'
+       '3.1.0   Release Date :: 2024-03-21'
   character(len = 50) :: dt_time
   character(len=72), dimension(10) :: id
 
