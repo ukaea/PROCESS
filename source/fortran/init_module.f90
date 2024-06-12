0987aa36724f6c72255a04276ffd6289a5cf6033
Michael Kovari <michael.kovari@ukaea.uk>
2024-06-12 11:24:12 +0100

    Removed two obsolete comments

--- source/fortran/init_module.f90
+++ source/fortran/init_module.f90
@@ -110,12 +110,6 @@ contains
       !! the default values for the global variables, reads in data from
       !! the input file, and checks the run parameters for consistency.
 
-
-      !! file:///home/mkumar/process/source/fortran/divertor_ode.f90
-
-      !
-      ! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!file:///home/mkumar/process/source/fortran/input.f90
-
       use global_variables, only: verbose, fileprefix, output_prefix
       use main_module, only: run_summary
       use constants, only: opt_file, vfile, nout, nplot, mfile, sig_file
