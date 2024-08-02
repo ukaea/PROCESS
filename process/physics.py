b9ee8e75ab0072b27ce3531f1ba8b4cc37379b98
Christopher Ashe- UKAEA <91618944+chris-ashe@users.noreply.github.com>
2024-08-02 11:01:17 +0100

    Shortened membership testing for iprofile

--- process/physics.py
+++ process/physics.py
@@ -2171,7 +2171,7 @@ class Physics:
             alphaj = qstar / q0 - 1.0
             rli = np.log(1.65 + 0.89 * alphaj)
 
-        if iprofile == 4 or iprofile == 5 or iprofile == 6:
+        if iprofile in {4, 5, 6}:
             # Spherical Tokamak relation for internal inductance
             # Menard et al. (2016), Nuclear Fusion, 56, 106023
             rli = 3.4 - kappa
