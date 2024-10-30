4b791455611c79a33e57644ac31a4cb08db88844
Jonathan Maddock <78556175+jonmaddock@users.noreply.github.com>
2024-10-30 14:53:26 +0000

    Write non-idempotence warning to OUT.DAT

--- process/final.py
+++ process/final.py
@@ -3,9 +3,10 @@
 from process import fortran as ft
 from process.fortran import final_module as fm
 from process import output as op
+from process.fortran import process_output as po
 
 
-def finalise(models, ifail):
+def finalise(models, ifail, non_idempotent_msg: None | str = None):
     """Routine to print out the final point in the scan.
 
     Writes to OUT.DAT and MFILE.DAT.
@@ -14,12 +15,19 @@ def finalise(models, ifail):
     :type models: process.main.Models
     :param ifail: error flag
     :type ifail: int
+    :param non_idempotent_msg: warning about non-idempotent variables, defaults to None
+    :type non_idempotent_msg: None | str, optional
     """
     fm.final_header(ifail)
 
     # Output relevant to no optimisation
     if ft.numerics.ioptimz == -2:
         fm.no_optimisation()
+
+    # Print non-idempotence warning to OUT.DAT only
+    if non_idempotent_msg:
+        po.oheadr(ft.constants.nout, "NON-IDEMPOTENT VARIABLES")
+        po.ocmmnt(ft.constants.nout, non_idempotent_msg)
 
     # Write output to OUT.DAT and MFILE.DAT
     op.write(models, ft.constants.nout)
