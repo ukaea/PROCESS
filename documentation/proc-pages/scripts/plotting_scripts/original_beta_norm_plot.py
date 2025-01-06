6ba94069b46f0cbfab46cb88dc16a48c2c9ec22a
mn3981 <christopher.ashe@ukaea.uk>
2025-01-06 10:05:47 +0000

    Add original normalized beta limit plotting script and fix spacing in Menard plot

--- documentation/proc-pages/scripts/plotting_scripts/original_beta_norm_plot.py
+++ documentation/proc-pages/scripts/plotting_scripts/original_beta_norm_plot.py
@@ -4,15 +4,15 @@ from bokeh.plotting import figure, output_file, save
 
 
 x = np.linspace(1.0, 5, 500)
-y = 3.12 + 3.5 * (1 / x) ** 1.7
+y = 2.7 * (1 + 5 * (1 / x) ** 3.5)
 source = ColumnDataSource(data=dict(x=x, y=y))
 
 plot = figure(
     x_range=(1, 5),
-    y_range=(2, 8),
+    y_range=(2, 15),
     width=400,
     height=400,
-    title="Menard Normalized Beta Limit",
+    title="Original Normalized Beta Limit",
 )
 plot.xaxis.axis_label = r"Aspect ratio, \  $$[A]$$"
 plot.yaxis.axis_label = r"Normalized beta limit, \  $$[\beta_N]$$"
@@ -20,5 +20,5 @@ plot.yaxis.axis_label = r"Normalized beta limit, \  $$[\beta_N]$$"
 plot.line("x", "y", source=source, line_width=3, line_alpha=0.6)
 
 # Save the plot as HTML
-output_file("menard_beta_norm.html", title="Menard Normalized Beta Limit")
+output_file("original_beta_norm.html", title="Original Normalized Beta Limit")
 save(plot)
