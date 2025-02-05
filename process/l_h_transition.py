f3950496634f6e399b176e897974949669bdac76
mn3981 <christopher.ashe@ukaea.uk>
2025-02-05 09:55:33 +0000

    :sparkle: Add new iter_1996_upper scaling function

--- process/l_h_transition.py
+++ process/l_h_transition.py
@@ -1,6 +1,6 @@
 def calculate_iter1996_nominal(dene20: float, bt: float, rmajor: float) -> float:
     """
-    Calculate the ITER-1996 L-H transition power threshold.
+    Calculate the nominal ITER-1996 L-H transition power threshold.
 
     :param dene20: Volume averaged electron density in units of 10^20 m^-3.
     :type dene20: float
@@ -22,3 +22,29 @@ def calculate_iter1996_nominal(dene20: float, bt: float, rmajor: float) -> float
         doi: https://doi.org/10.1063/1.872406.
     """
     return 0.45 * dene20**0.75 * bt * rmajor**2
+
+
+def calculate_iter1996_upper(dene20: float, bt: float, rmajor: float) -> float:
+    """
+    Calculate the upper variant ITER-1996 L-H transition power threshold.
+
+    :param dene20: Volume averaged electron density in units of 10^20 m^-3.
+    :type dene20: float
+    :param bt: Toroidal magnetic field [T]
+    :type bt: float
+    :param rmajor: Plasma major radius [m]
+    :type rmajor: float
+    :return: The ITER-1996 L-H transition power threshold [MW]
+    :rtype: float
+
+    :notes:
+
+    :references:
+        - T. Takizuka and International Atomic Energy Agency, Vienna (Austria),
+        "Threshold power and energy confinement for ITER". 1996.
+
+        - J. C. Wesley, “International Thermonuclear Experimental Reactor: Physics issues, capabilities and physics program plans,”
+        Physics of Plasmas, vol. 4, no. 7, pp. 2642-2652, Jul. 1997,
+        doi: https://doi.org/10.1063/1.872406.
+    """
+    return 0.3960502816 * dene20 * bt * rmajor**2.5
