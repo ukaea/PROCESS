c0df82d2ae77a8a0fc97cf3ddb7486510a26f36d
mn3981 <christopher.ashe@ukaea.uk>
2025-02-05 09:35:53 +0000

    :sparkle: Create new l_h_tranition file for storing all of the threshold scalings.

---
+++ process/l_h_transition.py
@@ -0,0 +1,24 @@
+def calculate_iter1996_nominal(dene20: float, bt: float, rmajor: float) -> float:
+    """
+    Calculate the ITER-1996 L-H transition power threshold.
+
+    :param dene20: Volume averaged electron density in units of 10^20 m^-3.
+    :type dene20: float
+    :param bt: Toroidal magnetic field [T]
+    :type bt: float
+    :param rmajor: Plasma major radius [m]
+    :type rmajor: float
+    :return: The ITER-1996 L-H transition power threshold [MW]
+    :rtype: float
+
+    :notes:
+
+    :references:
+        - T. Takizuka and International Atomic Energy Agency, Vienna (Austria),
+        "Threshold power and energy confinement for ITER". 1996.
+
+        - J. C. Wesley, “International Thermonuclear Experimental Reactor: Physics issues, capabilities and physics program plans,”
+        Physics of Plasmas, vol. 4, no. 7, pp. 2642-2652, Jul. 1997,
+        doi: https://doi.org/10.1063/1.872406.
+    """
+    return 0.45 * dene20**0.75 * bt * rmajor**2
