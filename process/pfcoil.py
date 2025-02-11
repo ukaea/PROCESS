a6133c95d55cf8f3087a48c9fbb868a68be6c6db
mn3981 <christopher.ashe@ukaea.uk>
2025-02-11 16:29:37 +0000

    🔄 Rename 'bmaxoh0' to 'b_cs_peak_pulse_start' across multiple files for consistency and clarity

--- process/pfcoil.py
+++ process/pfcoil.py
@@ -1161,8 +1161,8 @@ class PFCoil:
         bohco = abs(bzo)
 
         # Peak field at the Beginning-Of-Pulse (BOP)
-        # Occurs at inner edge of coil; bmaxoh0 and bzi are of same sign at BOP
-        pfv.bmaxoh0 = self.bfmax(
+        # Occurs at inner edge of coil; b_cs_peak_pulse_start and bzi are of same sign at BOP
+        pfv.b_cs_peak_pulse_start = self.bfmax(
             pfv.j_cs_pulse_start,
             pfv.r_pf_coil_inner[pfv.n_cs_pf_coils - 1],
             pfv.r_pf_coil_outer[pfv.n_cs_pf_coils - 1],
@@ -1171,10 +1171,12 @@ class PFCoil:
         timepoint = 2
         bri, bro, bzi, bzo = self.peakb(pfv.n_cs_pf_coils, 99, timepoint)
 
-        pfv.bmaxoh0 = abs(pfv.bmaxoh0 + bzi)
+        pfv.b_cs_peak_pulse_start = abs(pfv.b_cs_peak_pulse_start + bzi)
 
         # Maximum field values
-        pfv.b_pf_coil_peak[pfv.n_cs_pf_coils - 1] = max(pfv.bmaxoh, abs(pfv.bmaxoh0))
+        pfv.b_pf_coil_peak[pfv.n_cs_pf_coils - 1] = max(
+            pfv.bmaxoh, abs(pfv.b_cs_peak_pulse_start)
+        )
         pf.bpf2[pfv.n_cs_pf_coils - 1] = max(bohco, abs(bzo))
 
         # Stress ==> cross-sectional area of supporting steel to use
@@ -1298,7 +1300,7 @@ class PFCoil:
             # Allowable coil overall current density at BOP
 
             (jcritwp, pfv.jcableoh_bop, pfv.jscoh_bop, tmarg2) = self.superconpf(
-                pfv.bmaxoh0,
+                pfv.b_cs_peak_pulse_start,
                 pfv.vfohc,
                 pfv.fcuohsu,
                 (abs(pfv.c_pf_cs_coils_peak_ma[pfv.n_cs_pf_coils - 1]) / pfv.awpoh)
@@ -1654,7 +1656,7 @@ class PFCoil:
         epsilon = r / a
 
         # Field at inner radius of coil [T]
-        b_a = pfv.bmaxoh0
+        b_a = pfv.b_cs_peak_pulse_start
 
         # Field at outer radius of coil [T]
         # Assume to be 0 for now
@@ -2064,8 +2066,8 @@ class PFCoil:
                 op.ovarre(
                     self.outfile,
                     "Maximum field at Beginning Of Pulse (T)",
-                    "(bmaxoh0)",
-                    pfv.bmaxoh0,
+                    "(b_cs_peak_pulse_start)",
+                    pfv.b_cs_peak_pulse_start,
                     "OP ",
                 )
                 op.ovarre(
