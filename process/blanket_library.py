37e77cc14e427182a21ce4cb9a32bbd90f6cb4f1
mn3981 <christopher.ashe@ukaea.uk>
2025-02-18 13:46:07 +0000

    🔄 Rename bzfllengo to len_blkt_outboard_channel_total for clarity and consistency across the codebase

--- process/blanket_library.py
+++ process/blanket_library.py
@@ -728,7 +728,7 @@ class BlanketLibrary:
             fwbs_variables.bzfllengi_n_rad * blanket_library.bldepti
             + fwbs_variables.bzfllengi_n_pol * blanket_library.bllengi
         )
-        blanket_library.bzfllengo = (
+        blanket_library.len_blkt_outboard_channel_total = (
             fwbs_variables.bzfllengo_n_rad * blanket_library.bldepto
             + fwbs_variables.bzfllengo_n_pol * blanket_library.bllengo
         )
@@ -800,7 +800,7 @@ class BlanketLibrary:
                 np.pi
                 * fwbs_variables.radius_fw_channel
                 * fwbs_variables.radius_fw_channel
-                * blanket_library.bzfllengo
+                * blanket_library.len_blkt_outboard_channel_total
             )
             npblkto_liq = (
                 fwbs_variables.nopipes
@@ -875,7 +875,7 @@ class BlanketLibrary:
                 np.pi
                 * fwbs_variables.radius_fw_channel
                 * fwbs_variables.radius_fw_channel
-                * blanket_library.bzfllengo
+                * blanket_library.len_blkt_outboard_channel_total
             )
             npblkto_liq = (
                 fwbs_variables.nopipes
@@ -964,7 +964,7 @@ class BlanketLibrary:
                 np.pi
                 * fwbs_variables.radius_fw_channel
                 * fwbs_variables.radius_fw_channel
-                * blanket_library.bzfllengo
+                * blanket_library.len_blkt_outboard_channel_total
             )
 
             # Mass flow rate per coolant pipe
@@ -1049,7 +1049,7 @@ class BlanketLibrary:
             output,
             icoolpump=1,
             flow_velocity=blanket_library.velblkto,
-            flleng=blanket_library.bzfllengo,
+            flleng=blanket_library.len_blkt_outboard_channel_total,
             no90=no90bz,
             no180=no180bz,
             coolant_density=fwbs_variables.den_blkt_coolant,
