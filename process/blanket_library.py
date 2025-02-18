edc75c706fbfe2ce43f5e013b1dfb7e1311d2d9d
mn3981 <christopher.ashe@ukaea.uk>
2025-02-18 13:48:30 +0000

    🔄 Rename velblkto to vel_blkt_outboard_coolant for clarity and consistency across the codebase

--- process/blanket_library.py
+++ process/blanket_library.py
@@ -815,7 +815,7 @@ class BlanketLibrary:
             mfblktpo_liq = blanket_library.mfblkto_liq / npblkto_liq
             # Coolant velocites in blanket (m/s)
             # Assume BZ structure has same channel width as FW
-            blanket_library.velblkto = self.flow_velocity(
+            blanket_library.vel_blkt_outboard_coolant = self.flow_velocity(
                 i_channel_shape=1,
                 mass_flow_rate=blanket_library.mfblktpo,
                 flow_density=fwbs_variables.den_blkt_coolant,
@@ -890,7 +890,7 @@ class BlanketLibrary:
 
             # Coolant velocity in blanket (m/s)
             # Assume BZ structure has same channel width as FW
-            blanket_library.velblkto = self.flow_velocity(
+            blanket_library.vel_blkt_outboard_coolant = self.flow_velocity(
                 i_channel_shape=1,
                 mass_flow_rate=blanket_library.mfblktpo,
                 flow_density=fwbs_variables.den_blkt_coolant,
@@ -974,7 +974,7 @@ class BlanketLibrary:
 
             # Coolant velocity in blanket (m/s)
             # Assume BZ structure has same channel width as FW
-            blanket_library.velblkto = self.flow_velocity(
+            blanket_library.vel_blkt_outboard_coolant = self.flow_velocity(
                 i_channel_shape=1,
                 mass_flow_rate=blanket_library.mfblktpo,
                 flow_density=fwbs_variables.den_blkt_coolant,
@@ -1048,7 +1048,7 @@ class BlanketLibrary:
         deltap_blo = self.deltap_tot(
             output,
             icoolpump=1,
-            flow_velocity=blanket_library.velblkto,
+            flow_velocity=blanket_library.vel_blkt_outboard_coolant,
             flleng=blanket_library.len_blkt_outboard_channel_total,
             no90=no90bz,
             no180=no180bz,
