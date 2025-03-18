946bbce3c0b067239031afcca0aaa5f564e8f9ac
mn3981 <christopher.ashe@ukaea.uk>
2025-03-18 13:23:52 +0000

    :art: Refactor heat_transfer and darcy_friction_haaland methods to include roughness and radius parameters, and update related tests for improved clarity and functionality

--- tests/unit/test_fw.py
+++ tests/unit/test_fw.py
@@ -166,21 +166,21 @@ def test_fw_temp(fwtempparam, monkeypatch, fw):
     assert massrate == pytest.approx(fwtempparam.expected_massrate, rel=1e-4)
 
 
-def test_darcy_friction_haaland(monkeypatch, fw):
-    monkeypatch.setattr(fwbs_variables, "radius_fw_channel", 0.1)
-    monkeypatch.setattr(fwbs_variables, "roughness_fw_channel", 1e-6)
-
-    assert fw.darcy_friction_haaland(5500) == pytest.approx(0.0366668931278784)
+def test_darcy_friction_haaland(fw):
+    assert fw.darcy_friction_haaland(5500, 1e-6, 0.1) == pytest.approx(
+        0.0366668931278784
+    )
 
 
 def test_heat_transfer(fw):
     assert fw.heat_transfer(
-        masflx=112.19853108876258,
-        rhof=8.8673250601290707,
-        radius=0.0060000000000000001,
-        cf=5184.9330299967578,
-        viscf=4.0416219836935569e-05,
-        kf=0.3211653052986152,
+        mflux_coolant=112.19853108876258,
+        den_coolant=8.8673250601290707,
+        radius_channel=0.0060000000000000001,
+        heatcap_coolant=5184.9330299967578,
+        visc_coolant=4.0416219836935569e-05,
+        thermcond_coolant=0.3211653052986152,
+        roughness_fw_channel=6e-8,
     ) == pytest.approx(1929.221015448999)
 
 
