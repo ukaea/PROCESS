84afc7715f5dd508ac4ef5efaf634301e923e2ae
Timothy Nunn <timothy.nunn@ukaea.uk>
2025-06-05 12:55:14 +0000

    Pre-empt and guard against temp_margin root failure

--- process/superconducting_tf_coil.py
+++ process/superconducting_tf_coil.py
@@ -485,9 +485,8 @@ class SuperconductingTFCoil(TFCoil):
         # Temperature margin
         current_sharing_t = superconductors.current_sharing_rebco(bmax, jsc)
         tmarg = current_sharing_t - thelium
-        tfcoil_variables.temp_margin = (
-            tmarg  # Only used in the availabilty routine - see comment to Issue #526
-        )
+        # Only used in the availabilty routine - see comment to Issue #526
+        tfcoil_variables.temp_margin = tmarg
 
         if output:  # Output ----------------------------------
             total = (
@@ -1145,37 +1144,29 @@ class SuperconductingTFCoil(TFCoil):
 
         #  Temperature margin (already calculated in superconductors.bi2212 for isumat=2)
 
-        if (
-            (isumat == 1)
-            or (isumat == 3)
-            or (isumat == 4)
-            or (isumat == 5)
-            or (isumat == 7)
-            or (isumat == 8)
-            or (isumat == 9)
-        ):  # Find temperature at which current density margin = 0
+        if isumat in [1, 3, 4, 5, 7, 8, 9]:
+            # Find temperature at which current density margin = 0
             if isumat == 3:
                 arguments = (isumat, jsc, bmax, strain, bc20m, tc0m, c0)
             else:
                 arguments = (isumat, jsc, bmax, strain, bc20m, tc0m)
 
-            another_estimate = 2 * thelium
-            t_zero_margin, root_result = optimize.newton(
-                superconductors.current_density_margin,
-                thelium,
-                fprime=None,
-                args=arguments,
-                # args=(isumat, jsc, bmax, strain, bc20m, tc0m,),
-                tol=1.0e-06,
-                maxiter=50,
-                fprime2=None,
-                x1=another_estimate,
-                rtol=1.0e-6,
-                full_output=True,
-                disp=True,
-            )
-            # print(root_result)  # Diagnostic for newton method
-            tmarg = t_zero_margin - thelium
+            if superconductors.current_density_margin(1e-6, *arguments) < 0:
+                tmarg = -1.0
+            else:
+                root_result = optimize.root_scalar(
+                    superconductors.current_density_margin,
+                    x0=thelium,
+                    x1=2 * thelium,
+                    bracket=[1e-6, 500],
+                    args=arguments,
+                    xtol=1.0e-06,
+                    rtol=1.0e-6,
+                    maxiter=50,
+                )
+
+                tmarg = root_result.root - thelium
+
             tfcoil_variables.temp_margin = tmarg
 
         #  Find the current density limited by the protection limit
