82ab7c4471052d8004b6eebc5b431e095637e78a
Matti <matti.coleman@gmail.com>
2025-06-11 11:25:39 +0200

    typo

--- process/superconducting_tf_coil.py
+++ process/superconducting_tf_coil.py
@@ -10,7 +10,6 @@ from process.exceptions import ProcessValueError
 from process.fortran import (
     build_variables,
     constants,
-    constraint_variables,
     divertor_variables,
     error_handling,
     global_variables,
@@ -1331,21 +1330,23 @@ class SuperconductingTFCoil(TFCoil):
             temp_tf_coolant_peak_field,
             temp_tf_conductor_peak_quench,
         )
-        tfcoil_variables.jwdgpro, vd = self.protect_new(
-            iop,
-            tfes,
-            acs,
-            aturn,
-            tdump,
-            fcond,
-            fcu,
-            thelium,
-            tmax,
-            bmax,
-            tfcoil_variables.rrr_tf_cu,
-            tfcoil_variables.t_tf_quench_detection,
-            constraint_variables.nflutfmax,
-        )
+
+        # # At present only valid for LTS windings (Nb3Sn)
+        # tfcoil_variables.jwdgpro, vd = self.protect_new(
+        #     iop,
+        #     tfes,
+        #     acs,
+        #     aturn,
+        #     tdump,
+        #     fcond,
+        #     fcu,
+        #     thelium,
+        #     tmax,
+        #     bmax,
+        #     tfcoil_variables.rrr_tf_cu,
+        #     tfcoil_variables.t_tf_quench_detection,
+        #     constraint_variables.nflutfmax,
+        # )
 
         if output:  # Output --------------------------
             if tmarg <= 0.0e0:
