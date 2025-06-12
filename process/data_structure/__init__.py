59ee4816a2b4519bb3cbc79bed543e25bd68ecdf
Timothy Nunn <timothy.nunn@ukaea.uk>
2025-06-12 12:24:51 +0000

    Convert cost_variables to Python data structure

---
+++ process/data_structure/__init__.py
@@ -0,0 +1,13 @@
+from . import (
+    build_python_variables,
+    cost_2015_variables,
+    cost_variables,
+    power_variables,
+)
+
+__all__ = [
+    "build_python_variables",
+    "cost_2015_variables",
+    "cost_variables",
+    "power_variables",
+]
