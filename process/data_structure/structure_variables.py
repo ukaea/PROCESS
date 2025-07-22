bf4651816eb5b14eae05b078d0de9d08650ee5cb
Clair Mould <86794332+clmould@users.noreply.github.com>
2025-07-22 16:49:19 +0100

    convert structure_variables to python

---
+++ process/data_structure/structure_variables.py
@@ -0,0 +1,29 @@
+aintmass: float = None
+"""intercoil structure mass (kg)"""
+
+clgsmass: float = None
+"""gravity support structure for TF coil, PF coil and intercoil support systems (kg)"""
+
+coldmass: float = None
+"""total mass of components at cryogenic temperatures (kg)"""
+
+fncmass: float = None
+"""PF coil outer support fence mass (kg)"""
+
+gsmass: float = None
+"""reactor core gravity support mass (kg)"""
+
+
+def init_structure_variables():
+    """Initialise structure variables"""
+    global aintmass
+    global clgsmass
+    global coldmass
+    global fncmass
+    global gsmass
+
+    aintmass = 0.0
+    clgsmass = 0.0
+    coldmass = 0.0
+    fncmass = 0.0
+    gsmass = 0.0
