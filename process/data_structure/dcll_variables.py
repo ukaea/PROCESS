ee93baecc78b03f4c5f78a2eb767b24317742696
clmould <86794332+clmould@users.noreply.github.com>
2025-08-13 11:37:41 +0100

    Convert dcll data structure to python (#3785)
    * Convert dcll data structure to python
    
    * add to __init__

---
+++ process/data_structure/dcll_variables.py
@@ -0,0 +1,239 @@
+"""This module contains the Dual Coolant Lead Lithium (DCLL) specific submods of PROCESSS.
+
+author: G. Graham, CCFE
+
+Acronyms for this module:
+
+     BB          Breeding Blanket
+     FW          First Wall
+     BZ          Breeder Zone
+     MF/BSS      Manifold/Back Supporting Structure
+     LT          Low Temperature
+     HT          High Temperature
+     MMS         Multi Module Segment
+     SMS         Single Module Segment
+     IB          Inboard
+     OB          Outboard
+     HCD         Heating & Current Drive
+     FCI         Flow Channel Insert
+
+IN.DAT info for DCLL:
+
+     Select DCLL model
+         i_blanket_type = 5 * DCLL
+
+     Liquid Metal Breeder Material = PbLi
+         i_blkt_liquid_breeder_type = 0 * Liquid Metal Breeder Material = PbLi
+
+     Specify dual-coolant i.e., get mass flow required from heat extracted from liquid metal breeder
+         i_blkt_dual_coolant = 2
+
+     FIC switch: 0 = no FIC, Eurofer; 1 = FCIs, perfect electrical insulator, 2 = FCIs, with specified conductance
+         i_blkt_liquid_breeder_channel_type = 0, 1, or 2
+
+     Liquid metal duct wall conductance initialized at Eurofer value in fwbs_variables, or can input other value, used for i_blkt_liquid_breeder_channel_type = 0 or 2
+         (bz_channel_conduct_liq)
+
+     Choose if FW and BB structure are on the same pumping system (unless have different coolants), default is same coolant with flow IN->FW->BB->OUT
+         (i_fw_blkt_shared_coolant)
+
+     Can set inlet and outlet temperature for liquid metal breeder
+         (inlet_temp_liq)
+         (outlet_temp_liq)
+
+References:
+
+     [Nat1995]   Natesan et al. (1995), Assessment of alkali metal coolants for
+                 for the ITER blanket, Fusion Engineering and Design 27, 457-466
+
+     [Mal1995]   Malang and Mattas (1995), Comparison of lithium and the eutectic
+                 lead-lithium alloy, two candidate liquid metal breeder materials
+                 for self-cooled blankets, Fusion Engineering and Design 27, 399-406
+
+     [Gas2001]   Gasior and Mozer (2001), Thermodynamic study of liquid lithium-lead
+                 alloys using the EMF method, Journal of Nuclear Materials, 294, 77-83
+
+     [Pal2016]   Palermo et al. (2016), Neutronic analyses of the preliminary design
+                 of a DCLL blanket for the EUROfusion DEMO power plant,
+                 Fusion Engineering and Design 109-111.
+
+     [Gar2017]   Garcinuno et al. (2017), Design of a permeator against vacuum for
+                 tritium extraction from eutectic lithium-lead in a DCLL DEMO,
+                 Fusion Engineering and Design, 117, 226-231
+
+     [Fer2021]   Fernandez-Berceruelo et al. (2021), Alternatives for upgrading the
+                 eu dcll breeding blanket from mms to sms, Fusion Engineering and
+                 Design 167, 112380
+
+
+Note: request for when CCFE Bluemira neutronics work is added: output maximum values, as well as average values, for wall neutronics calculation if possible.
+"""
+
+r_fci: float = None
+"""Radial thickness of FCIs [m]"""
+
+r_backwall: float = None
+"""Radial thickness of backwall [m]"""
+
+bz_r_ib: float = None
+"""Radial BZ thickness [m]"""
+
+bz_r_ob: float = None
+"""Radial BZ thickness [m]"""
+
+f_vol_stff_plates: float = None
+"""Structure/coolant compositional fractions"""
+
+f_vol_stl_bz_struct: float = None
+"""Structure/coolant compositional fractions"""
+
+f_vol_stl_back_wall: float = None
+"""Structure/coolant compositional fractions"""
+
+f_vol_stl_fw: float = None
+"""Structure/coolant compositional fractions"""
+
+f_vol_mfbss_stl: float = None
+"""MF/BSS compositional fractions"""
+
+f_vol_mfbss_he: float = None
+"""MF/BSS compositional fractions"""
+
+f_vol_mfbss_pbli: float = None
+"""MF/BSS compositional fractions"""
+
+vol_fci: float = None
+"""Volume of FCIs [m^3]"""
+
+vol_bz_struct: float = None
+"""Volume of other BZ structure [m^3]"""
+
+vol_bz_liq: float = None
+"""Volume of liquid channels [m^3]"""
+
+vol_bz_liq_ib: float = None
+"""Volume of liquid channels [m^3]"""
+
+vol_bz_liq_ob: float = None
+"""Volume of liquid channels [m^3]"""
+
+vol_bw: float = None
+"""Volume of backwall [m^3]"""
+
+vol_bss: float = None
+"""Volume of MF/BSS [m^3]"""
+
+wht_cer: float = None
+"""BZ masses by composition [kg]"""
+
+wht_stl_struct: float = None
+"""BZ masses by composition [kg]"""
+
+wht_cool_struct: float = None
+"""BZ masses by composition [kg]"""
+
+wht_bw_stl: float = None
+"""Backwall masses by composition [kg]"""
+
+wht_bw_cool: float = None
+"""Backwall masses by composition [kg]"""
+
+wht_mfbss_stl: float = None
+"""MF/BSS masses by composition [kg]"""
+
+wht_mfbss_cool: float = None
+"""MF/BSS masses by composition [kg]"""
+
+wht_mfbss_pbli: float = None
+"""MF/BSS masses by composition [kg]"""
+
+fwmass_stl: float = None
+"""FW masses by composition [kg]"""
+
+fwmass_cool: float = None
+"""FW masses by composition [kg]"""
+
+mass_cool_blanket: float = None
+"""Total masses of material in blanket [kg]"""
+
+mass_liq_blanket: float = None
+"""Total masses of material in blanket [kg]"""
+
+mass_stl_blanket: float = None
+"""Total masses of material in blanket [kg]"""
+
+mass_segm_ib: float = None
+"""Total mass for an inboard/outboard reactor segment [kg]"""
+
+mass_segm_ob: float = None
+"""Total mass for an inboard/outboard reactor segment [kg]"""
+
+
+def init_dcll_module():
+    global r_fci
+    global r_backwall
+    global bz_r_ib
+    global bz_r_ob
+    global f_vol_stff_plates
+    global f_vol_stl_bz_struct
+    global f_vol_stl_back_wall
+    global f_vol_stl_fw
+    global f_vol_mfbss_stl
+    global f_vol_mfbss_he
+    global f_vol_mfbss_pbli
+    global vol_fci
+    global vol_bz_struct
+    global vol_bz_liq
+    global vol_bz_liq_ib
+    global vol_bz_liq_ob
+    global vol_bw
+    global vol_bss
+    global wht_cer
+    global wht_stl_struct
+    global wht_cool_struct
+    global wht_bw_stl
+    global wht_bw_cool
+    global wht_mfbss_stl
+    global wht_mfbss_cool
+    global wht_mfbss_pbli
+    global fwmass_stl
+    global fwmass_cool
+    global mass_cool_blanket
+    global mass_liq_blanket
+    global mass_stl_blanket
+    global mass_segm_ib
+    global mass_segm_ob
+
+    r_fci = 0.0
+    r_backwall = 0.0
+    bz_r_ib = 0.0
+    bz_r_ob = 0.0
+    f_vol_stff_plates = 0.0
+    f_vol_stl_bz_struct = 0.0
+    f_vol_stl_back_wall = 0.0
+    f_vol_stl_fw = 0.0
+    f_vol_mfbss_stl = 0.0
+    f_vol_mfbss_he = 0.0
+    f_vol_mfbss_pbli = 0.0
+    vol_fci = 0.0
+    vol_bz_struct = 0.0
+    vol_bz_liq = 0.0
+    vol_bz_liq_ib = 0.0
+    vol_bz_liq_ob = 0.0
+    vol_bw = 0.0
+    vol_bss = 0.0
+    wht_cer = 0.0
+    wht_stl_struct = 0.0
+    wht_cool_struct = 0.0
+    wht_bw_stl = 0.0
+    wht_bw_cool = 0.0
+    wht_mfbss_stl = 0.0
+    wht_mfbss_cool = 0.0
+    wht_mfbss_pbli = 0.0
+    fwmass_stl = 0.0
+    fwmass_cool = 0.0
+    mass_cool_blanket = 0.0
+    mass_liq_blanket = 0.0
+    mass_stl_blanket = 0.0
+    mass_segm_ib = 0.0
+    mass_segm_ob = 0.0
--- tests/unit/test_dcll.py
+++ tests/unit/test_dcll.py
@@ -2,11 +2,10 @@ from typing import Any, NamedTuple
 
 import pytest
 
-from process.data_structure import build_variables
+from process.data_structure import build_variables, dcll_variables
 from process.dcll import DCLL
 from process.fortran import (
     current_drive_variables,
-    dcll_module,
     fwbs_variables,
     physics_variables,
 )
@@ -941,85 +940,97 @@ def test_dcll_masses(dcllmassesparam, monkeypatch, dcll):
         dcllmassesparam.n_blkt_outboard_modules_toroidal,
     )
 
-    monkeypatch.setattr(dcll_module, "r_fci", dcllmassesparam.r_fci)
+    monkeypatch.setattr(dcll_variables, "r_fci", dcllmassesparam.r_fci)
 
-    monkeypatch.setattr(dcll_module, "r_backwall", dcllmassesparam.r_backwall)
+    monkeypatch.setattr(dcll_variables, "r_backwall", dcllmassesparam.r_backwall)
 
-    monkeypatch.setattr(dcll_module, "bz_r_ib", dcllmassesparam.bz_r_ib)
+    monkeypatch.setattr(dcll_variables, "bz_r_ib", dcllmassesparam.bz_r_ib)
 
-    monkeypatch.setattr(dcll_module, "bz_r_ob", dcllmassesparam.bz_r_ob)
+    monkeypatch.setattr(dcll_variables, "bz_r_ob", dcllmassesparam.bz_r_ob)
 
     monkeypatch.setattr(
-        dcll_module, "f_vol_stff_plates", dcllmassesparam.f_vol_stff_plates
+        dcll_variables, "f_vol_stff_plates", dcllmassesparam.f_vol_stff_plates
     )
 
     monkeypatch.setattr(
-        dcll_module, "f_vol_stl_bz_struct", dcllmassesparam.f_vol_stl_bz_struct
+        dcll_variables, "f_vol_stl_bz_struct", dcllmassesparam.f_vol_stl_bz_struct
     )
 
     monkeypatch.setattr(
-        dcll_module, "f_vol_stl_back_wall", dcllmassesparam.f_vol_stl_back_wall
+        dcll_variables, "f_vol_stl_back_wall", dcllmassesparam.f_vol_stl_back_wall
     )
 
-    monkeypatch.setattr(dcll_module, "f_vol_stl_fw", dcllmassesparam.f_vol_stl_fw)
-
-    monkeypatch.setattr(dcll_module, "f_vol_mfbss_stl", dcllmassesparam.f_vol_mfbss_stl)
-
-    monkeypatch.setattr(dcll_module, "f_vol_mfbss_he", dcllmassesparam.f_vol_mfbss_he)
+    monkeypatch.setattr(dcll_variables, "f_vol_stl_fw", dcllmassesparam.f_vol_stl_fw)
 
     monkeypatch.setattr(
-        dcll_module, "f_vol_mfbss_pbli", dcllmassesparam.f_vol_mfbss_pbli
-    )
-
-    monkeypatch.setattr(dcll_module, "vol_fci", dcllmassesparam.vol_fci)
-
-    monkeypatch.setattr(dcll_module, "vol_bz_struct", dcllmassesparam.vol_bz_struct)
-
-    monkeypatch.setattr(dcll_module, "vol_bz_liq", dcllmassesparam.vol_bz_liq)
-
-    monkeypatch.setattr(dcll_module, "vol_bz_liq_ib", dcllmassesparam.vol_bz_liq_ib)
-
-    monkeypatch.setattr(dcll_module, "vol_bz_liq_ob", dcllmassesparam.vol_bz_liq_ob)
-
-    monkeypatch.setattr(dcll_module, "vol_bw", dcllmassesparam.vol_bw)
-
-    monkeypatch.setattr(dcll_module, "vol_bss", dcllmassesparam.vol_bss)
-
-    monkeypatch.setattr(dcll_module, "wht_cer", dcllmassesparam.wht_cer)
-
-    monkeypatch.setattr(dcll_module, "wht_stl_struct", dcllmassesparam.wht_stl_struct)
-
-    monkeypatch.setattr(dcll_module, "wht_cool_struct", dcllmassesparam.wht_cool_struct)
-
-    monkeypatch.setattr(dcll_module, "wht_bw_stl", dcllmassesparam.wht_bw_stl)
-
-    monkeypatch.setattr(dcll_module, "wht_bw_cool", dcllmassesparam.wht_bw_cool)
-
-    monkeypatch.setattr(dcll_module, "wht_mfbss_stl", dcllmassesparam.wht_mfbss_stl)
-
-    monkeypatch.setattr(dcll_module, "wht_mfbss_cool", dcllmassesparam.wht_mfbss_cool)
-
-    monkeypatch.setattr(dcll_module, "wht_mfbss_pbli", dcllmassesparam.wht_mfbss_pbli)
-
-    monkeypatch.setattr(dcll_module, "fwmass_stl", dcllmassesparam.fwmass_stl)
-
-    monkeypatch.setattr(dcll_module, "fwmass_cool", dcllmassesparam.fwmass_cool)
-
-    monkeypatch.setattr(
-        dcll_module, "mass_cool_blanket", dcllmassesparam.mass_cool_blanket
+        dcll_variables, "f_vol_mfbss_stl", dcllmassesparam.f_vol_mfbss_stl
     )
 
     monkeypatch.setattr(
-        dcll_module, "mass_liq_blanket", dcllmassesparam.mass_liq_blanket
+        dcll_variables, "f_vol_mfbss_he", dcllmassesparam.f_vol_mfbss_he
     )
 
     monkeypatch.setattr(
-        dcll_module, "mass_stl_blanket", dcllmassesparam.mass_stl_blanket
+        dcll_variables, "f_vol_mfbss_pbli", dcllmassesparam.f_vol_mfbss_pbli
     )
 
-    monkeypatch.setattr(dcll_module, "mass_segm_ib", dcllmassesparam.mass_segm_ib)
+    monkeypatch.setattr(dcll_variables, "vol_fci", dcllmassesparam.vol_fci)
 
-    monkeypatch.setattr(dcll_module, "mass_segm_ob", dcllmassesparam.mass_segm_ob)
+    monkeypatch.setattr(dcll_variables, "vol_bz_struct", dcllmassesparam.vol_bz_struct)
+
+    monkeypatch.setattr(dcll_variables, "vol_bz_liq", dcllmassesparam.vol_bz_liq)
+
+    monkeypatch.setattr(dcll_variables, "vol_bz_liq_ib", dcllmassesparam.vol_bz_liq_ib)
+
+    monkeypatch.setattr(dcll_variables, "vol_bz_liq_ob", dcllmassesparam.vol_bz_liq_ob)
+
+    monkeypatch.setattr(dcll_variables, "vol_bw", dcllmassesparam.vol_bw)
+
+    monkeypatch.setattr(dcll_variables, "vol_bss", dcllmassesparam.vol_bss)
+
+    monkeypatch.setattr(dcll_variables, "wht_cer", dcllmassesparam.wht_cer)
+
+    monkeypatch.setattr(
+        dcll_variables, "wht_stl_struct", dcllmassesparam.wht_stl_struct
+    )
+
+    monkeypatch.setattr(
+        dcll_variables, "wht_cool_struct", dcllmassesparam.wht_cool_struct
+    )
+
+    monkeypatch.setattr(dcll_variables, "wht_bw_stl", dcllmassesparam.wht_bw_stl)
+
+    monkeypatch.setattr(dcll_variables, "wht_bw_cool", dcllmassesparam.wht_bw_cool)
+
+    monkeypatch.setattr(dcll_variables, "wht_mfbss_stl", dcllmassesparam.wht_mfbss_stl)
+
+    monkeypatch.setattr(
+        dcll_variables, "wht_mfbss_cool", dcllmassesparam.wht_mfbss_cool
+    )
+
+    monkeypatch.setattr(
+        dcll_variables, "wht_mfbss_pbli", dcllmassesparam.wht_mfbss_pbli
+    )
+
+    monkeypatch.setattr(dcll_variables, "fwmass_stl", dcllmassesparam.fwmass_stl)
+
+    monkeypatch.setattr(dcll_variables, "fwmass_cool", dcllmassesparam.fwmass_cool)
+
+    monkeypatch.setattr(
+        dcll_variables, "mass_cool_blanket", dcllmassesparam.mass_cool_blanket
+    )
+
+    monkeypatch.setattr(
+        dcll_variables, "mass_liq_blanket", dcllmassesparam.mass_liq_blanket
+    )
+
+    monkeypatch.setattr(
+        dcll_variables, "mass_stl_blanket", dcllmassesparam.mass_stl_blanket
+    )
+
+    monkeypatch.setattr(dcll_variables, "mass_segm_ib", dcllmassesparam.mass_segm_ib)
+
+    monkeypatch.setattr(dcll_variables, "mass_segm_ob", dcllmassesparam.mass_segm_ob)
 
     dcll.dcll_masses(False)
 
@@ -1065,112 +1076,120 @@ def test_dcll_masses(dcllmassesparam, monkeypatch, dcll):
 
     assert fwbs_variables.vfblkt == pytest.approx(dcllmassesparam.expected_vfblkt)
 
-    assert dcll_module.r_fci == pytest.approx(dcllmassesparam.expected_r_fci)
+    assert dcll_variables.r_fci == pytest.approx(dcllmassesparam.expected_r_fci)
 
-    assert dcll_module.r_backwall == pytest.approx(dcllmassesparam.expected_r_backwall)
+    assert dcll_variables.r_backwall == pytest.approx(
+        dcllmassesparam.expected_r_backwall
+    )
 
-    assert dcll_module.bz_r_ib == pytest.approx(dcllmassesparam.expected_bz_r_ib)
+    assert dcll_variables.bz_r_ib == pytest.approx(dcllmassesparam.expected_bz_r_ib)
 
-    assert dcll_module.bz_r_ob == pytest.approx(dcllmassesparam.expected_bz_r_ob)
+    assert dcll_variables.bz_r_ob == pytest.approx(dcllmassesparam.expected_bz_r_ob)
 
-    assert dcll_module.f_vol_stff_plates == pytest.approx(
+    assert dcll_variables.f_vol_stff_plates == pytest.approx(
         dcllmassesparam.expected_f_vol_stff_plates
     )
 
-    assert dcll_module.f_vol_stl_bz_struct == pytest.approx(
+    assert dcll_variables.f_vol_stl_bz_struct == pytest.approx(
         dcllmassesparam.expected_f_vol_stl_bz_struct
     )
 
-    assert dcll_module.f_vol_stl_back_wall == pytest.approx(
+    assert dcll_variables.f_vol_stl_back_wall == pytest.approx(
         dcllmassesparam.expected_f_vol_stl_back_wall
     )
 
-    assert dcll_module.f_vol_stl_fw == pytest.approx(
+    assert dcll_variables.f_vol_stl_fw == pytest.approx(
         dcllmassesparam.expected_f_vol_stl_fw
     )
 
-    assert dcll_module.f_vol_mfbss_stl == pytest.approx(
+    assert dcll_variables.f_vol_mfbss_stl == pytest.approx(
         dcllmassesparam.expected_f_vol_mfbss_stl
     )
 
-    assert dcll_module.f_vol_mfbss_he == pytest.approx(
+    assert dcll_variables.f_vol_mfbss_he == pytest.approx(
         dcllmassesparam.expected_f_vol_mfbss_he
     )
 
-    assert dcll_module.f_vol_mfbss_pbli == pytest.approx(
+    assert dcll_variables.f_vol_mfbss_pbli == pytest.approx(
         dcllmassesparam.expected_f_vol_mfbss_pbli
     )
 
-    assert dcll_module.vol_fci == pytest.approx(dcllmassesparam.expected_vol_fci)
+    assert dcll_variables.vol_fci == pytest.approx(dcllmassesparam.expected_vol_fci)
 
-    assert dcll_module.vol_bz_struct == pytest.approx(
+    assert dcll_variables.vol_bz_struct == pytest.approx(
         dcllmassesparam.expected_vol_bz_struct
     )
 
-    assert dcll_module.vol_bz_liq == pytest.approx(dcllmassesparam.expected_vol_bz_liq)
+    assert dcll_variables.vol_bz_liq == pytest.approx(
+        dcllmassesparam.expected_vol_bz_liq
+    )
 
-    assert dcll_module.vol_bz_liq_ib == pytest.approx(
+    assert dcll_variables.vol_bz_liq_ib == pytest.approx(
         dcllmassesparam.expected_vol_bz_liq_ib
     )
 
-    assert dcll_module.vol_bz_liq_ob == pytest.approx(
+    assert dcll_variables.vol_bz_liq_ob == pytest.approx(
         dcllmassesparam.expected_vol_bz_liq_ob
     )
 
-    assert dcll_module.vol_bw == pytest.approx(dcllmassesparam.expected_vol_bw)
+    assert dcll_variables.vol_bw == pytest.approx(dcllmassesparam.expected_vol_bw)
 
-    assert dcll_module.vol_bss == pytest.approx(dcllmassesparam.expected_vol_bss)
+    assert dcll_variables.vol_bss == pytest.approx(dcllmassesparam.expected_vol_bss)
 
-    assert dcll_module.wht_cer == pytest.approx(dcllmassesparam.expected_wht_cer)
+    assert dcll_variables.wht_cer == pytest.approx(dcllmassesparam.expected_wht_cer)
 
-    assert dcll_module.wht_stl_struct == pytest.approx(
+    assert dcll_variables.wht_stl_struct == pytest.approx(
         dcllmassesparam.expected_wht_stl_struct
     )
 
-    assert dcll_module.wht_cool_struct == pytest.approx(
+    assert dcll_variables.wht_cool_struct == pytest.approx(
         dcllmassesparam.expected_wht_cool_struct
     )
 
-    assert dcll_module.wht_bw_stl == pytest.approx(dcllmassesparam.expected_wht_bw_stl)
+    assert dcll_variables.wht_bw_stl == pytest.approx(
+        dcllmassesparam.expected_wht_bw_stl
+    )
 
-    assert dcll_module.wht_bw_cool == pytest.approx(
+    assert dcll_variables.wht_bw_cool == pytest.approx(
         dcllmassesparam.expected_wht_bw_cool
     )
 
-    assert dcll_module.wht_mfbss_stl == pytest.approx(
+    assert dcll_variables.wht_mfbss_stl == pytest.approx(
         dcllmassesparam.expected_wht_mfbss_stl
     )
 
-    assert dcll_module.wht_mfbss_cool == pytest.approx(
+    assert dcll_variables.wht_mfbss_cool == pytest.approx(
         dcllmassesparam.expected_wht_mfbss_cool
     )
 
-    assert dcll_module.wht_mfbss_pbli == pytest.approx(
+    assert dcll_variables.wht_mfbss_pbli == pytest.approx(
         dcllmassesparam.expected_wht_mfbss_pbli
     )
 
-    assert dcll_module.fwmass_stl == pytest.approx(dcllmassesparam.expected_fwmass_stl)
+    assert dcll_variables.fwmass_stl == pytest.approx(
+        dcllmassesparam.expected_fwmass_stl
+    )
 
-    assert dcll_module.fwmass_cool == pytest.approx(
+    assert dcll_variables.fwmass_cool == pytest.approx(
         dcllmassesparam.expected_fwmass_cool
     )
 
-    assert dcll_module.mass_cool_blanket == pytest.approx(
+    assert dcll_variables.mass_cool_blanket == pytest.approx(
         dcllmassesparam.expected_mass_cool_blanket
     )
 
-    assert dcll_module.mass_liq_blanket == pytest.approx(
+    assert dcll_variables.mass_liq_blanket == pytest.approx(
         dcllmassesparam.expected_mass_liq_blanket
     )
 
-    assert dcll_module.mass_stl_blanket == pytest.approx(
+    assert dcll_variables.mass_stl_blanket == pytest.approx(
         dcllmassesparam.expected_mass_stl_blanket
     )
 
-    assert dcll_module.mass_segm_ib == pytest.approx(
+    assert dcll_variables.mass_segm_ib == pytest.approx(
         dcllmassesparam.expected_mass_segm_ib
     )
 
-    assert dcll_module.mass_segm_ob == pytest.approx(
+    assert dcll_variables.mass_segm_ob == pytest.approx(
         dcllmassesparam.expected_mass_segm_ob
     )
