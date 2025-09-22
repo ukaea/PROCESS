3b7c0089de2a63ca0763e3534b87954f16b0d545
chris-ashe <christopher.ashe@ukaea.uk>
2025-09-22 21:03:30 +0100

    🔄 - Changed variable name from `bvert` to `b_plasma_vertical` in `physics_variables.py` for clarity and consistency.

--- process/pfcoil.py
+++ process/pfcoil.py
@@ -386,7 +386,7 @@ class PFCoil:
                         )
 
                 # Vertical field (T)
-                pv.bvert = (
+                pv.b_plasma_vertical = (
                     -1.0e-7
                     * pv.plasma_current
                     / pv.rmajor
@@ -506,7 +506,7 @@ class PFCoil:
                     )
                 )
 
-                pv.bvert = bzin[0]
+                pv.b_plasma_vertical = bzin[0]
 
                 ssqef, pfcoil_variables.ccls0 = self.efc(
                     npts0,
