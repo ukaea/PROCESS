ade4543e2f6a7661b05cf85b3594bac95577dff8
Timothy <75321887+timothy-nunn@users.noreply.github.com>
2025-09-23 09:52:56 +0100

    Allow numpy versions >=2 (#3855)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup_kwargs = {
     "test_suite": "pytest",
     "python_requires": ">=3.10",
     "install_requires": [
-        "numpy>=1.23,<2",
+        "numpy>=1.23",
         "scipy>=1.10",
         "cvxpy!=1.3.0,!=1.3.1",
         "osqp>=1.0",
