bae181deb7c2937942ea30477c205e886c9b1742
mn3981 <christopher.ashe@ukaea.uk>
2025-09-24 16:30:19 +0100

    Add total ion pressure profile calculations and plotting functionality

--- process/data_structure/physics_variables.py
+++ process/data_structure/physics_variables.py
@@ -809,7 +809,7 @@ pres_plasma_on_axis: float = None
 pres_plasma_electron_profile: list[float] = None
 """Profile of electron pressure in plasma (Pa)"""
 
-pres_plasma_ion_profile: list[float] = None
+pres_plasma_ion_total_profile: list[float] = None
 """Profile of ion pressure in plasma (Pa)"""
 
 j_plasma_on_axis: float = None
@@ -1482,7 +1482,7 @@ def init_physics_variables():
     global m_s_limit
     global pres_plasma_on_axis
     global pres_plasma_electron_profile
-    global pres_plasma_ion_profile
+    global pres_plasma_ion_total_profile
     global j_plasma_on_axis
     global f_dd_branching_trit
     global pden_plasma_alpha_mw
@@ -1731,7 +1731,7 @@ def init_physics_variables():
     m_s_limit = 0.3
     pres_plasma_on_axis = 0.0
     pres_plasma_electron_profile = []
-    pres_plasma_ion_profile = []
+    pres_plasma_ion_total_profile = []
     j_plasma_on_axis = 0.0
     f_dd_branching_trit = 0.0
     pden_plasma_alpha_mw = 0.0
