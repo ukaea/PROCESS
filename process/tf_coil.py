f0500e2deb91c83d9f8e052d77c8a93272ca4cdd
Timothy Nunn <timothy.nunn@ukaea.uk>
2025-10-03 13:02:00 +0100

    Scale linear system of equations in plane_stress and skip test if incompatible

--- process/tf_coil.py
+++ process/tf_coil.py
@@ -5066,12 +5066,23 @@ def plane_stress(nu, rad, ey, j, nlayers, n_radial_array):
     # out of numba, running the code, and then copying the result
     # back in. This means that the linear algebra solve is not compiled and runs
     # as if it were written in normal Python.
-    # This is necessary because numba compiles against the SciPy algebra library,
-    # not the Numpy one. There are differences in the Scipy solvers depending on
-    # operating system/architecture/Scipy version that cause tests to fail.
+    # This is necessary because it uses mpmath solvers that cannot be numba compiled.
+    # We do this because scipy and numpy linear algebra solvers produce
+    # slightly different results depending on the system (e.g. Mac and Linux) which mean
+    # PROCESS can produce different results depending on where it is run!
     # https://github.com/ukaea/PROCESS/issues/3027
     # https://github.com/scipy/scipy/issues/23639
     with numba.objmode(cc="float64[:]"):
+        # These matrices can often end up being very ill-conditioned which can lead to numerical
+        # instability when solving below.
+        # Scaling the matrix can help reduce numerical instability by reducing the condition of matrix.
+        # Here, we scale aa such that the largest element on a given row is 1.0. This does not
+        # change the solution provided each element of a given row is scaled by the same scalar
+        # and the corresponding entry in bb is also scaled the same amount.
+        row_scale = np.max(np.abs(aa), axis=1)
+        aa /= np.repeat(row_scale[:, np.newaxis], aa.shape[0], axis=1)
+        bb /= row_scale
+
         cc = np.linalg.solve(aa, bb)
 
     #  Multiply c by (-1) (John Last, internal CCFE memorandum, 21/05/2013)
