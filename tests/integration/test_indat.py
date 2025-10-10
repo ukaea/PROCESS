39f79f39b06eb753da5eb7f34e6d6591305e0e88
Timothy Nunn <timothy.nunn@ukaea.uk>
2025-10-10 15:30:45 +0100

    Add test to check re-written input files are the same as the source

--- tests/integration/test_indat.py
+++ tests/integration/test_indat.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from process.io.in_dat import INVariable
+from process.io.in_dat import InDat, INVariable
 
 
 @pytest.mark.parametrize("value", ["1", "1.0", "1,2,3", "1.0, 2.0", "string"])
@@ -16,3 +16,12 @@ def test_invariable_equality(value):
     v2 = INVariable(name, value, v_type, parameter_group, "different comment")
 
     assert v1 == v2
+
+
+def test_rewritten_indat_identical(temp_data_cwd):
+    indat = InDat(filename=str(temp_data_cwd / "large_tokamak_IN.DAT"))
+    indat.write_in_dat("new.IN.DAT")
+
+    new_indat = InDat(filename=str(temp_data_cwd / "new.IN.DAT"))
+
+    assert indat.data == new_indat.data
