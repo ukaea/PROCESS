7e23e50beba56e7710366c49c55f0f7196375bd0
mn3981 <christopher.ashe@ukaea.uk>
2025-10-10 13:29:30 +0100

    Add alpha plasma velocity profile variable and update initialization

--- process/data_structure/physics_variables.py
+++ process/data_structure/physics_variables.py
@@ -350,6 +350,9 @@ vel_plasma_deuteron_profile: list[float] = None
 vel_plasma_triton_profile: list[float] = None
 """triton plasma velocity profile (m/s)"""
 
+vel_plasma_alpha_profile: list[float] = None
+"""alpha plasma velocity profile (m/s)"""
+
 plasma_coulomb_log_electron_electron_profile: float = None
 """electron-electron coulomb logarithm profile"""
 
@@ -1433,6 +1436,7 @@ def init_physics_variables():
     global vel_plasma_electron_profile
     global vel_plasma_deuteron_profile
     global vel_plasma_triton_profile
+    global vel_plasma_alpha_profile
     global plasma_coulomb_log_electron_electron_profile
     global plasma_coulomb_log_electron_deuteron_profile
     global plasma_coulomb_log_electron_triton_profile
@@ -1694,6 +1698,7 @@ def init_physics_variables():
     vel_plasma_electron_profile = []
     vel_plasma_deuteron_profile = []
     vel_plasma_triton_profile = []
+    vel_plasma_alpha_profile = []
     plasma_coulomb_log_electron_electron_profile = []
     plasma_coulomb_log_electron_deuteron_profile = []
     plasma_coulomb_log_electron_triton_profile = []
