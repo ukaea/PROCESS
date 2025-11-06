4c1373d395f6423bc784a4abf8dff51e4bada951
ocean <OceanWongUK@gmail.com>
2025-11-06 17:42:07 +0000

    Used numpy testing in appropriate places

--- tests/unit/test_neutronics_data.py
+++ tests/unit/test_neutronics_data.py
@@ -45,9 +45,11 @@ def test_n2n_matrix_neg1():
     fibo_gs = np.array([8, 5, 3, 2, 1])
     n2n_matrix = n2n_weight_matrix(fibo_gs, -1)
     row_sum = n2n_matrix.sum(axis=1)
-    assert np.isclose(
-        row_sum, [1, 1, 1, np.log(2 / 1.5) / np.log(2 / 1)], rtol=0
-    ).all(), "Expected bin 4 to be partially out-of-bounds"
+    np.testing.assert_allclose(
+        row_sum,
+        [1, 1, 1, np.log(2 / 1.5) / np.log(2 / 1)],
+        err_msg="Expected bin 4 to be partially out-of-bounds",
+    )
     assert (n2n_matrix >= 0).all(), "Must be all non-negative."
 
 
@@ -55,8 +57,8 @@ def test_n2n_matrix_neg2():
     fibo_gs = np.array([8, 5, 3, 2, 1])
     n2n_matrix = n2n_weight_matrix(fibo_gs, -2)
     row_sum = n2n_matrix.sum(axis=1)
-    assert np.isclose(row_sum, [1, 1, 1, 0], rtol=0).all(), (
-        "Expected bin 4 to be out-of-bounds"
+    np.testing.assert_allclose(
+        row_sum, [1, 1, 1, 0], err_msg="Expected bin 4 to be out-of-bounds"
     )
     assert (n2n_matrix >= 0).all(), "Must be all non-negative."
 
@@ -65,9 +67,11 @@ def test_n2n_matrix_neg3():
     fibo_gs = np.array([8, 5, 3, 2, 1])
     n2n_matrix = n2n_weight_matrix(fibo_gs, -3)
     row_sum = n2n_matrix.sum(axis=1)
-    assert np.isclose(
-        row_sum, [1, 1, np.log(3 / 2.5) / np.log(3 / 2), 0], rtol=0
-    ).all(), "Expected bin 3 to be partially out-of-bounds"
+    np.testing.assert_allclose(
+        row_sum,
+        [1, 1, np.log(3 / 2.5) / np.log(3 / 2), 0],
+        err_msg="Expected bin 3 to be partially out-of-bounds",
+    )
     assert (n2n_matrix >= 0).all(), "Must be all non-negative."
 
 
@@ -75,9 +79,11 @@ def test_n2n_matrix_pos3():
     fibo_gs = np.array([8, 5, 3, 2, 1])
     n2n_matrix = n2n_weight_matrix(fibo_gs, 3)
     row_sum = n2n_matrix.sum(axis=1)
-    assert np.isclose(
-        row_sum, [np.log(6.5 / 5) / np.log(8 / 5), 1, 1, 1], rtol=0
-    ).all(), "Expected bin 1 to be partially out-of-bounds"
+    np.testing.assert_allclose(
+        row_sum,
+        [np.log(6.5 / 5) / np.log(8 / 5), 1, 1, 1],
+        err_msg="Expected bin 1 to be partially out-of-bounds",
+    )
     assert (n2n_matrix >= 0).all(), "Must be all non-negative."
 
 
@@ -85,7 +91,9 @@ def test_n2n_matrix_pos6():
     fibo_gs = np.array([8, 5, 3, 2, 1])
     n2n_matrix = n2n_weight_matrix(fibo_gs, 6)
     row_sum = n2n_matrix.sum(axis=1)
-    assert np.isclose(row_sum, [0, 1, 1, 1], rtol=0).all(), (
-        "Expected bin 1 to be completely out-of-bounds"
+    np.testing.assert_allclose(
+        row_sum,
+        [0, 1, 1, 1],
+        err_msg="Expected bin 1 to be completely out-of-bounds",
     )
     assert (n2n_matrix >= 0).all(), "Must be all non-negative."
