5c3a1f3bc16e50b36b2045d72bfc2bdfead5a94d
ocean <OceanWongUK@gmail.com>
2025-11-20 15:57:08 +0000

    Pre-commit ruff fix

--- tests/regression/test_neutronics.py
+++ tests/regression/test_neutronics.py
@@ -1,8 +1,8 @@
-import pytest
 import numpy as np
 
 from process.neutronics import NeutronFluxProfile
 from process.neutronics_data import MaterialMacroInfo
+
 
 def test_one_group():
     """
@@ -16,21 +16,25 @@ def test_one_group():
     sigma_fw_t = 1 / mfp_fw_t  # [1/m]
     sigma_fw_s = 1 / mfp_fw_s  # [1/m]
     a_fw = 52
-    fw_material = MaterialMacroInfo(dummy, a_fw, [sigma_fw_t], [[sigma_fw_s]], name="fw")
+    fw_material = MaterialMacroInfo(
+        dummy, a_fw, [sigma_fw_t], [[sigma_fw_s]], name="fw"
+    )
 
     mfp_bz_s = 97 * 0.01  # [m]
     mfp_bz_t = 35.8 * 0.01  # [m]
     sigma_bz_s = 1 / mfp_bz_s  # [1/m]
     sigma_bz_t = 1 / mfp_bz_t  # [1/m]
     a_bz = 71
-    bz_material = MaterialMacroInfo(dummy, a_bz, [sigma_bz_t], [[sigma_bz_s]], name="bz")
+    bz_material = MaterialMacroInfo(
+        dummy, a_bz, [sigma_bz_t], [[sigma_bz_s]], name="bz"
+    )
 
     x_fw, x_bz = 5.72 * 0.01, 85 * 0.01
     incoming_flux = 41
     neutron_profile = NeutronFluxProfile(
         incoming_flux, x_fw, x_bz, fw_material, bz_material
     )
-    
+
     const = neutron_profile.integration_constants[0]  # alias to fit line width
     assert np.isclose(const.fw_c[0], 80.5346770788), "c5"
     assert np.isclose(const.fw_s[0], -76.5562120985), "c6"
@@ -46,11 +50,13 @@ def test_one_group():
 
     fw_removal = sigma_fw_t - sigma_fw_s - fw_material.sigma_in[0, 0]
     bz_removal = sigma_bz_t - sigma_bz_s - bz_material.sigma_in[0, 0]
-    assert np.isclose(neutron_profile.flux,
+    assert np.isclose(
+        neutron_profile.flux,
         neutron_profile.neutron_current_escaped()
         + fw_removal * neutron_profile.integrated_flux_fw()
-        + bz_removal * neutron_profile.integrated_flux_bz()
+        + bz_removal * neutron_profile.integrated_flux_bz(),
     ), "Conservation of neutrons"
+
 
 def test_one_group_with_fission():
     """
@@ -64,7 +70,9 @@ def test_one_group_with_fission():
     sigma_fw_t = 1 / mfp_fw_t  # [1/m]
     sigma_fw_s = 1 / mfp_fw_s  # [1/m]
     a_fw = 52
-    fw_material = MaterialMacroInfo(dummy, a_fw, [sigma_fw_t], [[sigma_fw_s]], name="fw")
+    fw_material = MaterialMacroInfo(
+        dummy, a_fw, [sigma_fw_t], [[sigma_fw_s]], name="fw"
+    )
 
     mfp_bz_s = 97 * 0.01  # [m]
     mfp_bz_t = 35.8 * 0.01  # [m]
@@ -74,31 +82,49 @@ def test_one_group_with_fission():
 
     g = 1.2
     nu_sigma_bz_f = g * (sigma_bz_t - sigma_bz_s)
-    bz_material = MaterialMacroInfo(dummy, a_bz, [sigma_bz_t], [[sigma_bz_s]], [[nu_sigma_bz_f]], name="bz")
+    bz_material = MaterialMacroInfo(
+        dummy, a_bz, [sigma_bz_t], [[sigma_bz_s]], [[nu_sigma_bz_f]], name="bz"
+    )
 
     x_fw, x_bz = 5.72 * 0.01, 85 * 0.01
     incoming_flux = 41
     neutron_profile = NeutronFluxProfile(
         incoming_flux, x_fw, x_bz, fw_material, bz_material
     )
-    assert np.isclose(neutron_profile.l_bz_2[0], -(58.2869567709/100)**2)
-    assert np.isclose(neutron_profile.neutron_flux_at(-4.79675/100), 159.9434), "Minimum flux in FW"
-    assert np.isclose(neutron_profile.neutron_flux_at(4.79675/100), 159.9434), "Minimum flux in FW"
-    assert np.isclose(neutron_profile.neutron_flux_at(18.96382/100), 164.81245), "Maximum flux in BZ"
-    assert np.isclose(neutron_profile.neutron_flux_at(-18.96382/100), 164.81245), "Maximum flux in BZ"
-    assert np.isclose(neutron_profile.neutron_flux_fw(x_fw), neutron_profile.neutron_flux_bz(x_fw)), "Flux continuity assurance"
-    assert np.isclose(neutron_profile.neutron_current_fw2bz(), -7.6275782637960745), "Negative current because BZ (breeding) is backflowing into the FW"
-    assert np.isclose(neutron_profile.neutron_current_escaped(), 30.665951670177186), "positive escaped current."
+    assert np.isclose(neutron_profile.l_bz_2[0], -((58.2869567709 / 100) ** 2))
+    assert np.isclose(
+        neutron_profile.neutron_flux_at(-4.79675 / 100), 159.9434
+    ), "Minimum flux in FW"
+    assert np.isclose(
+        neutron_profile.neutron_flux_at(4.79675 / 100), 159.9434
+    ), "Minimum flux in FW"
+    assert np.isclose(
+        neutron_profile.neutron_flux_at(18.96382 / 100), 164.81245
+    ), "Maximum flux in BZ"
+    assert np.isclose(
+        neutron_profile.neutron_flux_at(-18.96382 / 100), 164.81245
+    ), "Maximum flux in BZ"
+    assert np.isclose(
+        neutron_profile.neutron_flux_fw(x_fw),
+        neutron_profile.neutron_flux_bz(x_fw),
+    ), "Flux continuity assurance"
+    assert np.isclose(
+        neutron_profile.neutron_current_fw2bz(), -7.6275782637960745
+    ), "Negative current because BZ (breeding) is backflowing into the FW"
+    assert np.isclose(
+        neutron_profile.neutron_current_escaped(), 30.665951670177186
+    ), "positive escaped current."
     fw_removal = sigma_fw_t - sigma_fw_s - fw_material.sigma_in[0, 0]
     bz_removal = sigma_bz_t - sigma_bz_s - bz_material.sigma_in[0, 0]
-    assert np.isclose(neutron_profile.flux,
+    assert np.isclose(
+        neutron_profile.flux,
         neutron_profile.neutron_current_escaped()
         + fw_removal * neutron_profile.integrated_flux_fw()
-        + bz_removal * neutron_profile.integrated_flux_bz()
+        + bz_removal * neutron_profile.integrated_flux_bz(),
     ), "Conservation of neutrons"
+
 
 def test_two_groups():
     """
-    Same group n=0 values as test_one_group. Second group can have a massive removal cross-section 
+    Same group n=0 values as test_one_group. Second group can have a massive removal cross-section
     """
-
