202efba124fc3f0e62ca93a2cf16e86e67057b0c
ocean <OceanWongUK@gmail.com>
2025-11-21 19:04:15 +0000

    Completed conversion from 2-layer model to infinite-layer model. Cleanup successful: unit test and regression test (1-group regression test case only) passed. Only the plotting function remains to be fixed for this conversion to infinite-layer model.

--- tests/regression/test_neutronics.py
+++ tests/regression/test_neutronics.py
@@ -32,20 +32,20 @@ def test_one_group():
     x_fw, x_bz = 5.72 * 0.01, 85 * 0.01
     incoming_flux = 41
     neutron_profile = NeutronFluxProfile(
-        incoming_flux, x_fw, x_bz, fw_material, bz_material
+        incoming_flux, [x_fw, x_bz], [fw_material, bz_material]
     )
 
-    const = neutron_profile.coefficients[0]  # alias to fit line width
-    assert np.isclose(const.fw_c[0], 80.5346770788), "c5"
-    assert np.isclose(const.fw_s[0], -76.5562120985), "c6"
-    assert np.isclose(const.bz_c[0], 60.6871656017), "c7"
-    assert np.isclose(const.bz_s[0], -60.7123696772), "c8"
+    const = neutron_profile.coefficients  # alias to fit line width
+    assert np.isclose(const[0][0].c[0], 80.5346770788), "c5"
+    assert np.isclose(const[0][0].s[0], -76.5562120985), "c6"
+    assert np.isclose(const[1][0].c[0], 60.6871656017), "c7"
+    assert np.isclose(const[1][0].s[0], -60.7123696772), "c8"
 
-    assert np.isclose(neutron_profile.neutron_flux_fw(x_fw), 48.72444)
-    assert np.isclose(neutron_profile.neutron_flux_bz(x_fw), 48.72444)
+    assert np.isclose(neutron_profile.neutron_flux_in_layer(0, x_fw), 48.72444)
+    assert np.isclose(neutron_profile.neutron_flux_in_layer(1, x_fw), 48.72444)
     assert np.isclose(neutron_profile.neutron_flux_at(x_fw), 48.72444)
 
-    assert np.isclose(neutron_profile.neutron_current_fw2bz(), 22.3980214162)
+    assert np.isclose(neutron_profile.neutron_current_through_interface(1), 22.3980214162)
     assert np.isclose(neutron_profile.neutron_current_escaped(), 1.22047369356)
 
     fw_removal = sigma_fw_t - sigma_fw_s - fw_material.sigma_in[0, 0]
@@ -53,8 +53,8 @@ def test_one_group():
     assert np.isclose(
         neutron_profile.flux,
         neutron_profile.neutron_current_escaped()
-        + fw_removal * neutron_profile.integrated_flux_fw()
-        + bz_removal * neutron_profile.integrated_flux_bz(),
+        + fw_removal * neutron_profile.integrated_flux_in_layer(0)
+        + bz_removal * neutron_profile.integrated_flux_in_layer(1),
     ), "Conservation of neutrons"
 
 
@@ -89,9 +89,9 @@ def test_one_group_with_fission():
     x_fw, x_bz = 5.72 * 0.01, 85 * 0.01
     incoming_flux = 41
     neutron_profile = NeutronFluxProfile(
-        incoming_flux, x_fw, x_bz, fw_material, bz_material
+        incoming_flux, [x_fw, x_bz], [fw_material, bz_material],
     )
-    assert np.isclose(neutron_profile.l_bz_2[0], -((58.2869567709 / 100) ** 2))
+    assert np.isclose(neutron_profile.l2[1][0], -((58.2869567709 / 100) ** 2))
     assert np.isclose(
         neutron_profile.neutron_flux_at(-4.79675 / 100), 159.9434
     ), "Minimum flux in FW"
@@ -105,11 +105,11 @@ def test_one_group_with_fission():
         neutron_profile.neutron_flux_at(-18.96382 / 100), 164.81245
     ), "Maximum flux in BZ"
     assert np.isclose(
-        neutron_profile.neutron_flux_fw(x_fw),
-        neutron_profile.neutron_flux_bz(x_fw),
+        neutron_profile.neutron_flux_in_layer(0, x_fw),
+        neutron_profile.neutron_flux_in_layer(1, x_fw),
     ), "Flux continuity assurance"
     assert np.isclose(
-        neutron_profile.neutron_current_fw2bz(), -7.6275782637960745
+        neutron_profile.neutron_current_through_interface(1), -7.6275782637960745
     ), "Negative current because BZ (breeding) is backflowing into the FW"
     assert np.isclose(
         neutron_profile.neutron_current_escaped(), 30.665951670177186
@@ -119,8 +119,8 @@ def test_one_group_with_fission():
     assert np.isclose(
         neutron_profile.flux,
         neutron_profile.neutron_current_escaped()
-        + fw_removal * neutron_profile.integrated_flux_fw()
-        + bz_removal * neutron_profile.integrated_flux_bz(),
+        + fw_removal * neutron_profile.integrated_flux_in_layer(0)
+        + bz_removal * neutron_profile.integrated_flux_in_layer(1),
     ), "Conservation of neutrons"
 
 
