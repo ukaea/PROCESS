907bde3387e7aca6a726eec55dd44f91df1f54ce
Timothy Nunn <timothy.nunn@ukaea.uk>
2025-11-25 16:54:22 +0000

    Add back warnings for fiooic, fjohc, and fjohc0

--- process/superconducting_tf_coil.py
+++ process/superconducting_tf_coil.py
@@ -1515,6 +1515,11 @@ class SuperconductingTFCoil(TFCoil):
             superconducting_tf_coil_variables.f_c_tf_turn_operating_critical,
             "OP ",
         )
+        if superconducting_tf_coil_variables.f_c_tf_turn_operating_critical > 0.7:
+            logger.error(
+                "f_c_tf_turn_operating_critical shouldn't be above 0.7 for engineering reliability"
+            )
+
         po.ovarre(
             self.outfile,
             "TF Superconductor quench dump time (s)",
