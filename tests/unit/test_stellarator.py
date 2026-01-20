f602e6c852d29a98f572387b493157a7205efa85
mn3981 <christopher.ashe@ukaea.uk>
2026-01-20 14:42:31 +0000

    Refactor beta information output to PlasmaBeta class for improved organization

--- tests/unit/test_stellarator.py
+++ tests/unit/test_stellarator.py
@@ -28,7 +28,7 @@ from process.data_structure import (
 )
 from process.fw import Fw
 from process.hcpb import CCFE_HCPB
-from process.physics import Physics
+from process.physics import Physics, PlasmaBeta
 from process.plasma_profiles import PlasmaProfile
 from process.power import Power
 from process.stellarator import Neoclassics, Stellarator
@@ -68,6 +68,7 @@ def stellarator():
                 LowerHybrid(plasma_profile=PlasmaProfile()),
                 ElectronBernstein(plasma_profile=PlasmaProfile()),
             ),
+            PlasmaBeta(),
         ),
         Neoclassics(),
     )
