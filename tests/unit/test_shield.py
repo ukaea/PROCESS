be1dc326b1b00fd403884aa5d0ae0abda6467b41
mn3981 <christopher.ashe@ukaea.uk>
2026-01-23 14:32:36 +0000

    Add unit test for elliptical shield areas calculation

--- tests/unit/test_shield.py
+++ tests/unit/test_shield.py
@@ -78,3 +78,65 @@ def test_elliptical_shield_volumes(shield, elliptical_shield_volumes, expected):
     assert vol_shield_inboard == expected[0]
     assert vol_shield_outboard == expected[1]
     assert vol_shield == expected[2]
+
+
+class EllipticalShieldAreas(NamedTuple):
+    rmajor: Any = None
+    rminor: Any = None
+    triang: Any = None
+    rsldi: Any = None
+    rsldo: Any = None
+    dz_shield_half: Any = None
+    dr_shield_inboard: Any = None
+    dr_shield_outboard: Any = None
+    dz_shield_upper: Any = None
+    dz_shield_lower: Any = None
+
+
+@pytest.mark.parametrize(
+    "elliptical_shield_areas, expected",
+    [
+        (
+            EllipticalShieldAreas(
+                rmajor=8,
+                rminor=2.6666666666666665,
+                triang=0.5,
+                rsldi=4.083333333333334,
+                rsldo=12.716666666666667,
+                dz_shield_half=6.8032752487304133,
+                dr_shield_inboard=0.30000000000000004,
+                dr_shield_outboard=0.80000000000000004,
+                dz_shield_upper=0.59999999999999998,
+            ),
+            (
+                pytest.approx(700.06731267447844),
+                pytest.approx(1344.1106481995357),
+                pytest.approx(2044.1779608740142),
+            ),
+        )
+    ],
+)
+def test_elliptical_shield_areas(shield, elliptical_shield_areas, expected):
+    """Tests `elliptical_shield_areas` function.
+    :param elliptical_shield_areas: input parameters for the function
+    :type elliptical_shield_areas: EllipticalShieldAreas
+
+
+    """
+
+    a_shield_inboard, a_shield_outboard, a_shield = (
+        shield.calculate_elliptical_shield_areas(
+            rsldi=elliptical_shield_areas.rsldi,
+            rsldo=elliptical_shield_areas.rsldo,
+            rmajor=elliptical_shield_areas.rmajor,
+            triang=elliptical_shield_areas.triang,
+            dr_shld_inboard=elliptical_shield_areas.dr_shield_inboard,
+            rminor=elliptical_shield_areas.rminor,
+            dz_shld_half=elliptical_shield_areas.dz_shield_half,
+            dr_shld_outboard=elliptical_shield_areas.dr_shield_outboard,
+        )
+    )
+
+    assert a_shield_inboard == expected[0]
+    assert a_shield_outboard == expected[1]
+    assert a_shield == expected[2]
