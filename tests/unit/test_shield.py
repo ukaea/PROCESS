297673025a6cdb25e96dfee27756a828402960a9
mn3981 <christopher.ashe@ukaea.uk>
2026-01-23 14:47:16 +0000

    Add unit test for D-shaped shield volumes calculation

--- tests/unit/test_shield.py
+++ tests/unit/test_shield.py
@@ -140,3 +140,75 @@ def test_elliptical_shield_areas(shield, elliptical_shield_areas, expected):
     assert a_shield_inboard == expected[0]
     assert a_shield_outboard == expected[1]
     assert a_shield == expected[2]
+
+
+class DShapedShieldVolumes(NamedTuple):
+    rminor: Any = None
+    rsldi: Any = None
+    dr_shld_inboard: Any = None
+    dr_fw_plasma_gap_inboard: Any = None
+    dr_fw_plasma_gap_outboard: Any = None
+    dr_fw_inboard: Any = None
+    dr_fw_outboard: Any = None
+    dr_blkt_inboard: Any = None
+    dr_blkt_outboard: Any = None
+    dz_shld_half: Any = None
+    dr_shld_outboard: Any = None
+    dz_shld_upper: Any = None
+
+
+@pytest.mark.parametrize(
+    "dshaped_shield_volumes, expected",
+    [
+        (
+            DShapedShieldVolumes(
+                rminor=2.5,
+                dr_shld_inboard=0.40000000000000002,
+                rsldi=1.5,
+                dr_fw_plasma_gap_inboard=0.10000000000000001,
+                dr_fw_plasma_gap_outboard=0.10000000000000001,
+                dr_fw_inboard=0.018000000000000002,
+                dr_fw_outboard=0.018000000000000002,
+                dr_blkt_inboard=0.0,
+                dr_blkt_outboard=1.0,
+                dz_shld_half=8.75,
+                dr_shld_outboard=0.30000000000000004,
+                dz_shld_upper=0.6000000000000009,
+            ),
+            (
+                pytest.approx(79.896984366095609),
+                pytest.approx(370.5642451119993),
+                pytest.approx(450.46122947809488),
+            ),
+        )
+    ],
+)
+def test_dshaped_shield_volumes(shield, dshaped_shield_volumes, expected):
+    """Tests `dshaped_shield_volumes` function.
+
+    :param dshaped_shield_volumes: input parameters for the function
+    :type dshaped_shield_volumes: DShapedShieldVolumes
+
+
+    """
+
+    vol_shield_inboard, vol_shield_outboard, vol_shield = (
+        shield.calculate_dshaped_shield_volumes(
+            rsldi=dshaped_shield_volumes.rsldi,
+            dr_shld_inboard=dshaped_shield_volumes.dr_shld_inboard,
+            dr_fw_inboard=dshaped_shield_volumes.dr_fw_inboard,
+            dr_fw_plasma_gap_inboard=dshaped_shield_volumes.dr_fw_plasma_gap_inboard,
+            rminor=dshaped_shield_volumes.rminor,
+            dr_fw_plasma_gap_outboard=dshaped_shield_volumes.dr_fw_plasma_gap_outboard,
+            dr_fw_outboard=dshaped_shield_volumes.dr_fw_outboard,
+            dr_blkt_inboard=dshaped_shield_volumes.dr_blkt_inboard,
+            dr_blkt_outboard=dshaped_shield_volumes.dr_blkt_outboard,
+            dz_shld_half=dshaped_shield_volumes.dz_shld_half,
+            dr_shld_outboard=dshaped_shield_volumes.dr_shld_outboard,
+            dz_shld_upper=dshaped_shield_volumes.dz_shld_upper,
+        )
+    )
+
+    assert vol_shield_inboard == expected[0]
+    assert vol_shield_outboard == expected[1]
+    assert vol_shield == expected[2]
