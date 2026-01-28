e06c52258c058271ba68abb90043848fe957b6c2
clmould <86794332+clmould@users.noreply.github.com>
2026-01-28 10:00:42 +0000

    Add a VaryRun example (#4048)
    * remove unused&outdated parts of the config file to avoid confusion
    
    * remove varyrun cell from examples.ipynb
    
    * Add varyrun example and necessary input files
    
    Move setup functions required for examples to a separate file to tidy up
    
    Add varyrun example test
    
    * remove print statements
    
    * move call to fn
    
    * changes based on review comments
    
    ignore the README.txt produced by varyrun in examples/data dir
    
    * fix test
    
    * change displaying of an example conf file
    
    * Update examples/vary_run_example.ipynb
    
    Co-authored-by: Timothy <75321887+timothy-nunn@users.noreply.github.com>
    
    * Update tests/examples/test_examples.py
    
    Co-authored-by: Timothy <75321887+timothy-nunn@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Timothy <75321887+timothy-nunn@users.noreply.github.com>

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -100,3 +100,14 @@ def test_single_model_evaluation(examples_temp_data):
     )
     with testbook(single_model_evaluation_notebook_location, execute=True, timeout=600):
         pass
+
+
+def test_varyrun_example(examples_temp_data):
+    """Run vary_run_example.ipynb and check no exceptions are raised.
+
+    :param examples_temp_data: temporary dir containing examples files
+    :type examples_temp_data: Path
+    """
+    varyrun_example_notebook_location = examples_temp_data / "vary_run_example.ipynb"
+    with testbook(varyrun_example_notebook_location, execute=True, timeout=600):
+        pass
