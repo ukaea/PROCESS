3ecbb7ba9122777e0f03b8a5e88898499bfb1c86
mn3981 <christopher.ashe@ukaea.uk>
2026-02-04 09:28:36 +0000

    Requested changes

--- process/fw.py
+++ process/fw.py
@@ -198,7 +198,23 @@ class FirstWall:
         a_fw_inboard_full_coverage: float,
         a_fw_outboard_full_coverage: float,
     ) -> tuple[float, float, float]:
-        """Apply first wall coverage factors to calculate actual first wall areas."""
+        """Apply first wall coverage factors to calculate actual first wall areas.
+
+        :param n_divertors: Number of divertors (1 or 2).
+        :type n_divertors: int
+        :param f_ster_div_single: Fractional area of first wall sterically blocked by single divertor.
+        :type f_ster_div_single: float
+        :param f_a_fw_outboard_hcd: Fractional area of outboard first wall covered by high heat flux components.
+        :type f_a_fw_outboard_hcd: float
+        :param a_fw_inboard_full_coverage: First wall inboard area assuming 100% coverage (m^2).
+        :type a_fw_inboard_full_coverage: float
+        :param a_fw_outboard_full_coverage: First wall outboard area assuming 100% coverage (m^2).
+        :type a_fw_outboard_full_coverage: float
+
+        :returns: Contains first wall inboard area, outboard area, and total area (m^2).
+        :rtype: tuple[float, float, float]
+
+        """
         if n_divertors == 2:
             # Double null configuration
             a_fw_outboard = a_fw_outboard_full_coverage * (
