3fc9e5d412a0f3b76f3f38aec58d1219daf09061
mn3981 <christopher.ashe@ukaea.uk>
2026-02-17 11:58:37 +0000

    Add upper hybrid frequency profile to physics variables

--- process/data_structure/physics_variables.py
+++ process/data_structure/physics_variables.py
@@ -1362,6 +1362,9 @@ freq_plasma_larmor_toroidal_deuteron_profile: list[float] = None
 freq_plasma_larmor_toroidal_triton_profile: list[float] = None
 """Profile of triton Larmor frequency in plasma due to toroidal magnetic field (Hz)"""
 
+freq_plasma_upper_hybrid_profile: list[float] = None
+"""Profile of upper hybrid frequency in plasma (Hz)"""
+
 
 def init_physics_module():
     """Initialise the physics module"""
@@ -1697,7 +1700,8 @@ def init_physics_variables():
         freq_plasma_deuteron_profile, \
         freq_plasma_larmor_toroidal_electron_profile, \
         freq_plasma_larmor_toroidal_deuteron_profile, \
-        freq_plasma_larmor_toroidal_triton_profile
+        freq_plasma_larmor_toroidal_triton_profile, \
+        freq_plasma_upper_hybrid_profile
 
     m_beam_amu = 0.0
     m_fuel_amu = 0.0
@@ -1974,3 +1978,4 @@ def init_physics_variables():
     freq_plasma_larmor_toroidal_electron_profile = []
     freq_plasma_larmor_toroidal_deuteron_profile = []
     freq_plasma_larmor_toroidal_triton_profile = []
+    freq_plasma_upper_hybrid_profile = []
