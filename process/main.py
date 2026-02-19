f8d31d8df6386a6e906068a6be7c5a3647d5ce4c
Timothy Nunn <timothy.nunn@ukaea.uk>
2026-02-19 15:03:13 +0000

    Move costs under models/costs

--- process/main.py
+++ process/main.py
@@ -49,7 +49,6 @@ import process
 import process.data_structure as data_structure
 import process.init as init
 from process import constants
-from process.costs import Costs
 from process.cryostat import Cryostat
 from process.cs_fatigue import CsFatigue
 from process.current_drive import (
@@ -89,6 +88,7 @@ from process.models.availability import Availability
 from process.models.blankets.blanket_library import BlanketLibrary
 from process.models.build import Build
 from process.models.buildings import Buildings
+from process.models.costs.costs import Costs
 from process.models.costs.costs_2015 import Costs2015
 from process.pfcoil import PFCoil
 from process.physics import DetailedPhysics, Physics, PlasmaBeta, PlasmaInductance
--- tests/unit/test_ife.py
+++ tests/unit/test_ife.py
@@ -5,7 +5,6 @@ from typing import Any, NamedTuple
 import numpy as np
 import pytest
 
-from process.costs import Costs
 from process.data_structure import (
     buildings_variables,
     cost_variables,
@@ -17,6 +16,7 @@ from process.data_structure import (
 )
 from process.ife import IFE
 from process.models.availability import Availability
+from process.models.costs.costs import Costs
 
 
 @pytest.fixture
