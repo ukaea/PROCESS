f8c62372ff9ca9570f7204123e53b27bce753870
Timothy Nunn <timothy.nunn@ukaea.uk>
2026-02-19 15:54:06 +0000

    Rename physics_functions to radiation_power

--- process/models/physics/physics.py
+++ process/models/physics/physics.py
@@ -12,7 +12,7 @@ import process.models.physics.confinement_time as confinement
 import process.models.physics.fusion_reactions as reactions
 import process.models.physics.impurity_radiation as impurity_radiation
 import process.models.physics.l_h_transition as transition
-import process.models.physics.physics_functions as physics_funcs
+import process.models.physics.radiation_power as physics_funcs
 from process import constants
 from process import process_output as po
 from process.data_structure import (
