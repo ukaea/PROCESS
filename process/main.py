87e20e8238b133ec9142d214e5f2424b97f47ab3
mn3981 <christopher.ashe@ukaea.uk>
2026-02-23 21:14:18 +0000

    post rebase structure update

--- process/main.py
+++ process/main.py
@@ -95,6 +95,7 @@ from process.models.physics.physics import (
     DetailedPhysics,
     Physics,
     PlasmaBeta,
+    PlasmaExhaust,
     PlasmaInductance,
 )
 from process.models.physics.plasma_geometry import PlasmaGeom
