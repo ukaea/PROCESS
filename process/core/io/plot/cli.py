b5169f66baf9fe385cdbfdac18c412a0baae493e
james <81617086+je-cook@users.noreply.github.com>
2026-02-24 14:39:31 +0000

    plot_scans

--- process/core/io/plot/cli.py
+++ process/core/io/plot/cli.py
@@ -3,8 +3,9 @@ from pathlib import Path
 import click
 
 from process.io.plot.plot_proc import setup_plot
+from process.io.plot.plot_scans import plot_scan
 from process.io.plot.plot_stress_tf import plot_stress
-from process.io.tools import LazyGroup, mfile_arg, mfile_opt
+from process.io.tools import LazyGroup, mfile_arg, mfile_opt, split_callback
 
 
 @click.group(
@@ -24,16 +25,20 @@ def plot():
 @click.option(
     "-yv",
     "--y-vars",
+    "output_names",
+    callback=split_callback,
     required=True,
     help=(
         "Select the output variables\nMore than one output can be plotted "
-        "eg: -yv 'var1 var2'\nA separate plot will be created for each "
+        "eg: -yv 'var1:var2'\nA separate plot will be created for each "
         "inputs"
     ),
 )
 @click.option(
     "-yv2",
     "--y-vars2",
+    "output_names2",
+    callback=split_callback,
     default="",
     help=(
         "Select the 2nd axis output variable\n "
@@ -45,33 +50,31 @@ def plot():
     "-o",
     "--outputdir",
     default=Path.cwd(),
+    type=click.Path(),
     help="Output directory for plots, defaults to current working directory.",
 )
 @click.option(
     "-out",
-    "--term_output",
+    "--term-output",
     is_flag=True,
     help="Option to show scans values on terminal",
 )
 @click.option(
     "-sf",
     "--save-format",
-    nargs="?",
     default="pdf",
     help="Output format (default='pdf') ",
 )
 @click.option(
     "-afs",
     "--axis-font-size",
-    nargs="?",
     default=18,
     help="Axis label font size selection (default=18)",
     type=int,
 )
 @click.option(
     "-ats",
-    "--axis-ticklabel-size",
-    nargs="?",
+    "--axis-tick-size",
     default=16,
     help="Axis tick label font size selection (default=16)",
     type=int,
@@ -85,14 +88,17 @@ def plot():
 @click.option(
     "-xm",
     "--x-axis-max",
+    callback=split_callback,
     default="",
     help=(
-        "Used to set the x value corresponding to 100 percent when \nconverting from absolute to percent values."
+        "Used to set the x value corresponding to 100 percent when "
+        "converting from absolute to percent values. Multiple values separated with :"
     ),
 )
 @click.option(
     "-xr",
     "--x-axis-range",
+    callback=split_callback,
     default="",
     help=("Used to set the range for x axis"),
 )
@@ -105,6 +111,7 @@ def plot():
 @click.option(
     "-y2%",
     "--y-axis2-percent",
+    "y_axis_percent2",
     is_flag=True,
     help=(
         "Used to set the y axis ticks to percentages in place of absolute \nvalues. For the twinned axis if present."
@@ -113,6 +120,7 @@ def plot():
 @click.option(
     "-ym",
     "--y-axis-max",
+    callback=split_callback,
     default="",
     help=(
         "Used to set the y value corresponding to 100 percent when \nconverting from absolute to percent values."
@@ -121,6 +129,7 @@ def plot():
 @click.option(
     "-ym2",
     "--y-axis2-max",
+    callback=split_callback,
     default="",
     help=(
         "Used to set the y value corresponding to 100 percent when \nconverting from absolute to percent values."
@@ -130,12 +139,15 @@ def plot():
 @click.option(
     "-yr",
     "--y-axis-range",
+    callback=split_callback,
     default="",
     help=("Used to set the range for y axis"),
 )
 @click.option(
     "-yr2",
     "--y-axis2-range",
+    "y_axis_range2",
+    callback=split_callback,
     default="",
     help=("Used to set the range for y axis. For the twinned axis if present."),
 )
@@ -143,6 +155,7 @@ def plot():
     "-ln",
     "--label-name",
     default="",
+    callback=split_callback,
     help=(
         "Label names for plot legend. If multiple input files used then \n"
         "list the same number of label names eg: -nl 'leg1 leg2'\n"
@@ -152,6 +165,7 @@ def plot():
 @click.option(
     "-2DC",
     "--two-dimensional-contour",
+    "twoD_contour",
     is_flag=True,
     help=(
         "Option to plot 2D scans as a coloured contour plot instead of a line plot \n  "
@@ -168,10 +182,51 @@ def plot():
         "Variables will be plotted in order of input"
     ),
 )
-def plot_scans():
+def plot_scans_cli(
+    mfiles,
+    output_names,
+    output_names2,
+    outputdir,
+    term_output,
+    save_format,
+    axis_font_size,
+    axis_tick_size,
+    x_axis_percent,
+    x_axis_max,
+    x_axis_range,
+    y_axis_percent,
+    y_axis_percent2,
+    y_axis_max,
+    y_axis2_max,
+    y_axis_range,
+    y_axis_range2,
+    label_name,
+    twod_contour,
+    stack_plots,
+):
     """Plot optimisation information"""
-
-    return parser.parse_args(args)
+    return plot_scan(
+        mfiles,
+        output_names,
+        output_names2,
+        outputdir,
+        term_output,
+        save_format,
+        axis_font_size,
+        axis_tick_size,
+        x_axis_percent,
+        list(map(float, x_axis_max)),
+        list(map(float, x_axis_range)),
+        y_axis_percent,
+        y_axis_percent2,
+        list(map(float, y_axis_max)),
+        list(map(float, y_axis2_max)),
+        list(map(float, y_axis_range)),
+        list(map(float, y_axis_range2)),
+        label_name,
+        twod_contour,
+        stack_plots,
+    )
 
 
 @plot.command("tf-stress", no_args_is_help=True)
