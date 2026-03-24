4674d2a07893c7b30f9f82dae7d15d0d8173dee7
mn3981 <christopher.ashe@ukaea.uk>
2026-03-24 15:08:08 +0000

    Update TF coil calculations to use `dr_tf_internal_midplane` for internal coil width and enhance documentation

--- process/models/tfcoil/base.py
+++ process/models/tfcoil/base.py
@@ -83,6 +83,7 @@ class TFCoil(Model):
             tfcoil_variables.dx_tf_inboard_out_toroidal,
             tfcoil_variables.a_tf_leg_outboard,
             tfcoil_variables.dr_tf_full_midplane,
+            tfcoil_variables.dr_tf_internal_midplane,
             tfcoil_variables.dr_tf_plasma_case,
             tfcoil_variables.dx_tf_side_case_min,
         ) = self.tf_global_geometry(
@@ -206,6 +207,7 @@ class TFCoil(Model):
             - **tan_theta_coil** (*float*): Tangent of the toroidal angular spacing.
             - **a_tf_inboard_total** (*float*): Cross-sectional area of the inboard leg of the TF coil [m²].
             - **dr_tf_full_midplane** (*float*): Full external coil width at mid-plane [m].
+            - **dr_tf_internal_midplane** (*float*): Full internal coil width at mid-plane [m].
             - **r_tf_outboard_in** (*float*): Inner radius of the outboard leg of the TF coil [m].
             - **r_tf_outboard_out** (*float*): Outer radius of the outboard leg of the TF coil [m].
             - **dx_tf_inboard_out_toroidal** (*float*): Width of the inboard leg at the outer edge in the toroidal direction [m].
@@ -251,6 +253,9 @@ class TFCoil(Model):
         # Full external coil width at mid-plane [m]
 
         dr_tf_full_midplane = r_tf_outboard_out - r_tf_inboard_in
+
+        # Full internal coil width at mid-plane [m]
+        dr_tf_internal_midplane = r_tf_outboard_in - r_tf_inboard_out
 
         # ======================================================================
         # Plasma facing front case thickness [m]
@@ -301,6 +306,7 @@ class TFCoil(Model):
             dx_tf_inboard_out_toroidal,
             a_tf_leg_outboard,
             dr_tf_full_midplane,
+            dr_tf_internal_midplane,
             dr_tf_plasma_case,
             dx_tf_side_case_min,
         )
@@ -762,6 +768,13 @@ class TFCoil(Model):
             "Full external coil width at mid-plane (m)",
             "(dr_tf_full_midplane)",
             tfcoil_variables.dr_tf_full_midplane,
+            "OP ",
+        )
+        po.ovarre(
+            self.outfile,
+            "Full internal coil width at mid-plane (m)",
+            "(dr_tf_internal_midplane)",
+            tfcoil_variables.dr_tf_internal_midplane,
             "OP ",
         )
         po.ovarre(
