955ffa5348cf1dd879e9ce6755ba28c810a1bb23
ym1906 <graeme.turkington@ukaea.uk>
2026-04-08 16:50:10 +0100

    fixed inputs to beam_fusion, plasma profile in the wrong place

--- process/models/physics/physics.py
+++ process/models/physics/physics.py
@@ -570,7 +570,7 @@ class Physics(Model):
 
         # Calculate neutral beam slowing down effects
         # If ignited, then ignore beam fusion effects
-
+        current_drive_variables.c_beam_total = 2.0
         if (current_drive_variables.c_beam_total != 0.0e0) and (  # noqa: RUF069
             physics_variables.i_plasma_ignited == 0
         ):
@@ -592,8 +592,8 @@ class Physics(Model):
                 physics_variables.f_plasma_fuel_tritium,
                 current_drive_variables.f_beam_tritium,
                 physics_variables.sigmav_dt_average,
-                self.plasma_profile,
                 physics_variables.temp_plasma_ion_density_weighted_kev,
+                self.plasma_profile,
                 physics_variables.vol_plasma,
                 physics_variables.n_charge_plasma_effective_mass_weighted_vol_avg,
             )
