82094548c3d65fedfd97a2352dff3645216588e3
ym1906 <graeme.turkington@ukaea.uk>
2026-04-09 10:49:18 +0100

    updated _pf_loss_busbar_j to numpy docstring

--- process/power.py
+++ process/power.py
@@ -122,20 +122,25 @@ class Power:
         Loss = Δt * sum_groups (I_mean^2 * R_bus)
         Ref: M. Kovari, "PF power supplies accounting 2, Issue #972"
 
-        :param idx_time_interval: index of time interval (n -> n+1)
-        :type idx_time_interval: int
-        :param dt_pulse_phase_s: duration of pulse interval [s]
-        :type dt_pulse_phase_s: float
-        :param n_pf_coil_groups: number of PF coil groups/circuits
-        :type n_pf_coil_groups: int
-        :param pf_group_circuit_index: mapping from PF group to representative circuit index
-        :type pf_group_circuit_index: np.ndarray
-        :param c_pf_coil_turn: PF circuit current per turn at pulse times [A]
-        :type c_pf_coil_turn: np.ndarray
-        :param pfbusr: PF busbar resistance for each circuit/group [ohm]
-        :type pfbusr: np.ndarray
-        :return: busbar electrical energy loss over interval [J]
-        :rtype: float
+        Parameters
+        ----------
+        idx_time_interval : int
+            index of time interval (n -> n+1)
+        dt_pulse_phase_s : float
+            duration of pulse interval [s]
+        n_pf_coil_groups : int
+            number of PF coil groups/circuits
+        pf_group_circuit_index : np.ndarray
+            mapping from PF group to representative circuit index
+        c_pf_coil_turn : np.ndarray
+            PF circuit current per turn at pulse times [A]
+        pfbusr : np.ndarray
+            PF busbar resistance for each circuit/group [ohm]
+
+        Returns
+        -------
+        float
+            busbar electrical energy loss over interval [J]
         """
         e_loss_pf_bus_j = 0.0e0
 
