94b6dde5181ed88f754ee6a40d62235625945e8a
james <81617086+je-cook@users.noreply.github.com>
2026-04-14 10:24:41 +0100

    more fixes

--- process/models/stellarator/coils/mass.py
+++ process/models/stellarator/coils/mass.py
@@ -17,11 +17,6 @@ def calculate_coils_mass(a_tf_wp_with_insulation: float, a_tf_wp_no_insulation: 
     a_tf_wp_no_insulation:
         Area of the toroidal field coil winding pack without insulation.
 
-    Returns
-    -------
-    :
-        The function performs calculations and updates external state.
-
     """
     #  Masses of conductor constituents
     casing()
