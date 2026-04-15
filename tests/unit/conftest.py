faacaac50db080351ebb1a7188ff026be5094d35
james <81617086+je-cook@users.noreply.github.com>
2026-04-15 16:32:00 +0100

    fix data paths

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -3,7 +3,9 @@
 Define fixtures that will be shared across unit test modules.
 """
 
+import os
 from pathlib import Path
+from shutil import copy
 
 import pytest
 
@@ -33,3 +35,42 @@ def input_file():
     input_file = data_path / "large_tokamak_IN.DAT"
     # Convert input file path to absolute and string
     return str(Path(input_file).resolve())
+
+
+@pytest.fixture
+def temp_data(tmp_path):
+    """Copy data dir contents into temp dir for testing.
+
+    Any changes are discarded on fixture teardown.
+    :param tmp_path: temporary path fixture
+    :type tmp_path: Path
+    :return: temporary path containing data files
+    :rtype: Path
+    """
+    data_path = Path(__file__).parent.parent / "integration" / "data"
+
+    for data_file in data_path.glob("*"):
+        dst = tmp_path / data_file.name
+        copy(data_file, dst)
+
+    # Return tmp_path, now containing files copied from data dir
+    return tmp_path
+
+
+@pytest.fixture
+def temp_data_cwd(temp_data):
+    """Change cwd to temp_data dir, then yield it.
+
+    Used when testing command-line args that look for files in the cwd.
+    :param temp_data: temporary path containing data files
+    :type temp_data: Path
+    :yield: temporary path containing data files
+    :rtype: Path
+    """
+    # Setup by changing cwd to temp_data and yielding it
+    old_wd = os.getcwd()
+    os.chdir(temp_data)
+    yield temp_data
+
+    # Teardown by changing back to previous dir
+    os.chdir(old_wd)
