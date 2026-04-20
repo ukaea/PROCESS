e1f66bfd2ba73bb3965a000dda6d121f09b1e78e
ym1906 <graeme.turkington@ukaea.uk>
2026-04-20 09:28:41 +0100

    Space nit-pick

--- process/core/io/plot/summary.py
+++ process/core/io/plot/summary.py
@@ -3680,8 +3680,6 @@ def toroidal_cross_section(
         # Centreline tangency point
         x_beam_centre, y_beam_centre = calc_xy(radius_beam_tangency)
         x0 = r4
-        y0_beam_inner = w + dx_beam_shield
-        y0_beam_outer = w + dx_beam_shield + dx_beam_duct
         y0_beam_centre = w + dx_beam_shield + 0.5 * dx_beam_duct
 
         # Draw beam duct boundaries
