f05b4c7e323e00c42f83cea1d9ccc33dac541468
mn3981 <christopher.ashe@ukaea.uk>
2026-04-28 16:05:46 +0100

    Add calculations for blanket poloidal plasma angles in DCLL class

--- process/models/blankets/dcll.py
+++ process/models/blankets/dcll.py
@@ -2,6 +2,7 @@ from process.core import constants
 from process.core import (
     process_output as po,
 )
+from process.data_structure import blanket_library as blanket_vars
 from process.data_structure import (
     build_variables,
     current_drive_variables,
@@ -96,6 +97,27 @@ class DCLL(InboardBlanket, OutboardBlanket):
 
     def run(self, output: bool = False):
         self.component_volumes()
+
+        # If Shfranov shift is added, the angle formula can be used where the shift is added to the minor radius. For now, the shift is neglected and the angle is calculated using the minor radius only.
+        blanket_vars.deg_blkt_outboard_poloidal_plasma = (
+            self.blkt_outboard_poloidal_plasma_angle
+        )
+        blanket_vars.f_deg_blkt_outboard_poloidal_plasma = (
+            self.f_deg_blkt_outboard_poloidal_plasma
+        )
+
+        blanket_vars.deg_blkt_inboard_poloidal_plasma = (
+            self.calculate_blkt_inboard_poloidal_plasma_angle(
+                rminor=physics_variables.rminor,
+                dz_blkt_half=blanket_vars.dz_blkt_half,
+                dr_fw_plasma_gap_inboard=build_variables.dr_fw_plasma_gap_inboard,
+            )
+        )
+
+        blanket_vars.f_deg_blkt_inboard_poloidal_plasma = (
+            blanket_vars.deg_blkt_inboard_poloidal_plasma / 360.0
+        )
+
         dia_blkt_channel = self.pipe_hydraulic_diameter(i_channel_shape=1)
         self.data.fwbs.radius_blkt_channel = dia_blkt_channel / 2
         (
