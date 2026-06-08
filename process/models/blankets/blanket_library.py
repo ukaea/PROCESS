23d5cb866720775af9d8b273dfda18642e84d075
Christopher Ashe <91618944+chris-ashe@users.noreply.github.com>
2026-06-08 15:05:07 +0100

    Update process/models/blankets/blanket_library.py
    Co-authored-by: clmould <86794332+clmould@users.noreply.github.com>

--- process/models/blankets/blanket_library.py
+++ process/models/blankets/blanket_library.py
@@ -796,7 +796,7 @@ class BlanketLibrary(Model):
                 self.outfile,
                 "Coolant type",
                 "(i_fw_coolant_type)",
-                self.data.fwbs.i_fw_coolant_type,
+                CoolantType(self.data.fwbs.i_fw_coolant_type).full_name,
             )
             po.ovarrf(
                 self.outfile,
