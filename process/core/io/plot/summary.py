6a1bb58be90fd344ea195e71b48b0cef5813bd47
mn3981 <christopher.ashe@ukaea.uk>
2026-06-08 14:38:53 +0100

    Update radiation and alpha particle heat flux descriptions for first wall surfaces

--- process/core/io/plot/summary.py
+++ process/core/io/plot/summary.py
@@ -15279,7 +15279,8 @@ def plot_poloidal_power_distribution(
     ax.text(
         label_x,
         label_y,
-        f"{deg_blkt_outboard_poloidal_plasma:.1f}°\n({f_deg_blkt_outboard_poloidal_plasma * 100:.1f}%)",
+        f"$P_{{\\gamma}}$={m_file.get('p_fw_outboard_rad_mw', scan=scan):.1f}MW\n"
+        f"$P_{{\\alpha}}$={m_file.get('p_fw_outboard_alpha_surface_mw', scan=scan):.1f}MW",
         fontsize=7,
         color="purple",
         ha="center",
@@ -15332,7 +15333,8 @@ def plot_poloidal_power_distribution(
     ax.text(
         label_x,
         label_y,
-        f"{deg_blkt_inboard_poloidal_plasma:.1f}°\n({f_deg_blkt_inboard_poloidal_plasma * 100:.1f}%)",
+        f"$P_{{\\gamma}}$={m_file.get('p_fw_inboard_rad_mw', scan=scan):.1f}MW\n"
+        f"$P_{{\\alpha}}$={m_file.get('p_fw_inboard_alpha_surface_mw', scan=scan):.1f}MW",
         fontsize=7,
         color="green",
         ha="center",
