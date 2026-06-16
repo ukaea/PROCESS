d587ca8e09d71661a00c5ffe7b589ddbb15b5a43
Graeme Turkington <107113942+grmtrkngtn@users.noreply.github.com>
2026-06-16 17:25:28 +0100

    4280 non existent variables (#4310)
    * Removed defunct variables from the output
    
    * Fixed blanket lifeitme variable name

--- process/models/stellarator/stellarator.py
+++ process/models/stellarator/stellarator.py
@@ -1550,57 +1550,9 @@ class Stellarator(Model):
                 )
                 po.ovarre(
                     self.outfile,
-                    "Nuclear heating on i/b coil (MW/m3)",
-                    "(pnuctfi)",
-                    self.data.fwbs.pnuctfi,
-                )
-                po.ovarre(
-                    self.outfile,
-                    "Nuclear heating on o/b coil (MW/m3)",
-                    "(pnuctfo)",
-                    self.data.fwbs.pnuctfo,
-                )
-                po.ovarre(
-                    self.outfile,
                     "Total nuclear heating on coil (MW)",
                     "(p_tf_nuclear_heat_mw)",
                     self.data.fwbs.p_tf_nuclear_heat_mw,
-                )
-                po.ovarre(
-                    self.outfile,
-                    "Fast neut. fluence on i/b coil (n/m2)",
-                    "(nflutfi)",
-                    self.data.fwbs.nflutfi * 1.0e4,
-                )
-                po.ovarre(
-                    self.outfile,
-                    "Fast neut. fluence on o/b coil (n/m2)",
-                    "(nflutfo)",
-                    self.data.fwbs.nflutfo * 1.0e4,
-                )
-                po.ovarre(
-                    self.outfile,
-                    "Minimum final He conc. in IB VV (appm)",
-                    "(vvhemini)",
-                    self.data.fwbs.vvhemini,
-                )
-                po.ovarre(
-                    self.outfile,
-                    "Minimum final He conc. in OB VV (appm)",
-                    "(vvhemino)",
-                    self.data.fwbs.vvhemino,
-                )
-                po.ovarre(
-                    self.outfile,
-                    "Maximum final He conc. in IB VV (appm)",
-                    "(vvhemaxi)",
-                    self.data.fwbs.vvhemaxi,
-                )
-                po.ovarre(
-                    self.outfile,
-                    "Maximum final He conc. in OB VV (appm)",
-                    "(vvhemaxo)",
-                    self.data.fwbs.vvhemaxo,
                 )
                 po.ovarre(
                     self.outfile,
@@ -1611,8 +1563,8 @@ class Stellarator(Model):
                 po.ovarre(
                     self.outfile,
                     "Blanket lifetime (calendar years)",
-                    "(t_bl_y)",
-                    self.data.fwbs.t_bl_y,
+                    "(life_blkt)",
+                    self.data.fwbs.life_blkt,
                 )
 
             if (self.data.heat_transport.ipowerflow == 1) and (
