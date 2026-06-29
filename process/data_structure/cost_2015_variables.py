04627bab766fea1322e4e3ed3150b14424a3c357
James Morris <46964268+jmorris-uk@users.noreply.github.com>
2026-06-29 19:56:58 +0100

    Update cost_2015_variables.py

--- process/data_structure/cost_2015_variables.py
+++ process/data_structure/cost_2015_variables.py
@@ -6,23 +6,23 @@ import numpy as np
 @dataclass(slots=True)
 class Cost2015Data:
 
-    # Average plant electrical output factoring in availability (MW)
+    """Average plant electrical output factoring in availability (MW)"""
     mean_electric_output: float = 0.0
 
-    # Plant annual electrical output (MW.hrs)
+    """Plant annual electrical output (MW.hrs)"""
     annual_electric_output: float = 0.0
 
-    # Annual maintenance cost (M$)
+    """Annual maintenance cost (M$)"""
     maintenance: float = 0.0
 
-    # Total capital cost (M$)
+    """Total capital cost (M$)"""
     total_costs: float = 0.0
 
     s_label: list[str] = field(
         default_factory=lambda: np.array(["not used"] * 100, dtype=object)
     )
 
-    # arrays for cost scaling values: k_ref, k, c_ref, cost, cost_factor
+    """arrays for cost scaling values: k_ref, k, c_ref, cost, cost_factor"""
     s_kref: list[float] = field(default_factory=lambda: np.zeros(100, dtype=np.float64))
 
     s_k: list[float] = field(default_factory=lambda: np.zeros(100, dtype=np.float64))
