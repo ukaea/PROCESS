a88831ce2674cb8c5f7ce77d81f2867a75237bb7
mn3981 <christopher.ashe@ukaea.uk>
2026-06-29 11:42:13 +0100

    Remove now obsolete variables for phyaux and implement new variables to previous functions and tests

--- process/data_structure/physics_variables.py
+++ process/data_structure/physics_variables.py
@@ -643,9 +643,6 @@ class PhysicsData:
     e_plasma_magnetic_stored: float = 0.0
     """Plasma stored magnetic energy [J]"""
 
-    burnup: float = 0.0
-    """fractional plasma burnup"""
-
     f_plasma_fuel_burnup: float = 0.0
     """Total fuel burnup fraction in plasma"""
 
@@ -1353,9 +1350,6 @@ class PhysicsData:
     """Plasma safety factor at 95% flux surface (q₉₅) (`iteration variable 18`)
     """
 
-    molflow_plasma_fuelling_required: float = 0.0
-    """plasma fuelling rate (nucleus-pairs/s)"""
-
     f_plasma_particles_lcfs_recycled: float = 0.9
     """Fraction of plasma particles that are recycled at the LCFS. Recycling coefficent (R)"""
 
@@ -1382,9 +1376,6 @@ class PhysicsData:
 
     f_molflow_plasma_fuelling_helium3: float = 0.0
     """Fraction of plasma fuelling that is helium-3"""
-
-    tauratio: float = 1.0
-    """tauratio /1.0/ : ratio of He and pellet particle confinement times"""
 
     q95_min: float = 0.0
     """Plasmalower limit for edge safety factor"""
@@ -1430,9 +1421,6 @@ class PhysicsData:
 
     f_nd_plasma_carbon_electron: float = 0.0
     """n_carbon / n_e"""
-
-    rndfuel: float = 0.0
-    """fuel burnup rate (reactions/second)"""
 
     f_nd_plasma_iron_argon_electron: float = 0.0
     """n_highZ / n_e"""
