10ae44087dae957d019cc985f730811ca4a57016
chris-ashe <christopher.ashe@ukaea.uk>
2026-06-29 23:53:21 +0100

    :bug: Make confinement time comparison output the global plasma confinement time instead of just the electron

--- process/models/physics/confinement_time.py
+++ process/models/physics/confinement_time.py
@@ -1501,7 +1501,7 @@ class PlasmaConfinementTime(Model):
             po.ocmmnt(
                 self.outfile,
                 f"{'':>2}{scaling_name:<38}"
-                f"{confinement_time_data.t_electron_energy_confinement:<28.3f}{self.data.physics.hfac[i_confinement_time - 1]:.3f}",
+                f"{confinement_time_data.t_plasma_energy_confinement:<28.3f}{self.data.physics.hfac[i_confinement_time - 1]:.3f}",
             )
 
         po.oblnkl(self.outfile)
