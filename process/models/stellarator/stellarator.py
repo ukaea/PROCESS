4834a37b748a21704782e527b4060319a2ba3a82
mn3981 <christopher.ashe@ukaea.uk>
2026-07-31 14:49:44 +0100

    Apply new fuelling constraints to large tokamak eval regression test

--- process/models/stellarator/stellarator.py
+++ process/models/stellarator/stellarator.py
@@ -2371,7 +2371,7 @@ class Stellarator(Model):
             self.data.physics.t_energy_confinement,
         )
 
-        self.data.physics.f_plasma_fuel_burnup = self.physics.fuelling.calculate_fuel_burnup_fraction(
+        self.data.physics.f_plasma_fuel_burnup = self.physics.fuelling.calculate_fuel_burnup_fraction(  # noqa: E501
             fusrat_total=self.data.physics.fusrat_total,
             molflow_plasma_fuelling_vv_injected=self.data.physics.molflow_plasma_fuelling_vv_injected,
             molflow_beam_deuterium_vv_injected=self.data.physics.molflow_beam_deuterium_vv_injected,
