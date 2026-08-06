Fix calculation of upstream SOL outboard parallel area and update corresponding test value

process/models/physics/scrape_off_layer.py
@@ -249,5 +249,5 @@ class ScrapeOffLayer(Model):
         return (
             (2 * np.pi * (rmajor + rminor))
             * len_plasma_sol_power_decay
-            * (b_plasma_outboard_total / b_plasma_surface_poloidal_average)
+            * (b_plasma_surface_poloidal_average / b_plasma_outboard_total)
         )