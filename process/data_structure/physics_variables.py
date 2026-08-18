69aae7102f20daba8cad03534c03d785f890311a
mn3981 <christopher.ashe@ukaea.uk>
2026-08-18 10:38:48 +0100

    Add Eich 2011 JET power decay length variables to PhysicsData class

--- process/data_structure/physics_variables.py
+++ process/data_structure/physics_variables.py
@@ -1709,6 +1709,12 @@ class PhysicsData:
     len_plasma_sol_mast14_power_decay_2: float = 0.0
     """MAST 2014 power decay length in the scrape-off layer scaling 2 (λ_q) [m]"""
 
+    len_plasma_sol_eich11_jet_power_decay: float = 0.0
+    """Eich 2011 JET power decay length in the scrape-off layer scaling (λ_q) [m]"""
+
+    len_plasma_sol_eich11_jet_asdex_power_decay: float = 0.0
+    """Eich 2011 JET + ASDEX power decay length in the scrape-off layer scaling (λ_q) [m]"""
+
     a_plasma_outboard_sol_eich13_parallel: float = 0.0
     """Plasma outboard midplane (upstream) Eich 2013 SOL parallel area (Aₗₗ,ᵤ) [m]"""
 
