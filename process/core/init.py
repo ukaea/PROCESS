29a12c468af2e91dc89f880b75d10f560829b7b6
Clair Mould <86794332+clmould@users.noreply.github.com>
2026-08-27 15:19:59 +0100

    move validate_input to InDat class and rename to check_obsolete_variables

--- process/core/init.py
+++ process/core/init.py
@@ -15,6 +15,7 @@ import process
 from process.core import constants, process_output
 from process.core.exceptions import ProcessValidationError
 from process.core.input import parse_input_file
+from process.core.io.in_dat.base import InDat
 from process.core.solver import iteration_variables
 from process.core.solver.constraints import ConstraintManager
 from process.data_structure.blanket_variables import BlktModelTypes
@@ -52,7 +53,7 @@ if TYPE_CHECKING:
     from process.core.data_structure.base import DataStructure
 
 
-def init_process(data: DataStructure):
+def init_process(data: DataStructure, update_obsolete: bool = False):
     """Routine that calls the initialisation routines
 
     This routine calls the main initialisation routines that set
@@ -65,7 +66,10 @@ def init_process(data: DataStructure):
     # Creating and open the files MFile and OUTFile
     process_output.OutputFileManager.open_files(data.globals.output_prefix)
 
-    # Input any desired new initial values
+    filename = data.globals.output_prefix + "IN.DAT"
+    # InDat reads in and updates obsolete variables if requested
+    in_dat = InDat(filename=filename, update_obsolete=update_obsolete)  # noqa: F841
+
     inputs = parse_input_file(data)
 
     # Set active constraints
