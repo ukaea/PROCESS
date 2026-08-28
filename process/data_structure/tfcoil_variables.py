0a5bfbbc879fb2212c12ceb8d88d40bff2de32ef
mn3981 <christopher.ashe@ukaea.uk>
2026-08-28 13:47:37 +0100

    Add inductance and charging time variables to TFData class

--- process/data_structure/tfcoil_variables.py
+++ process/data_structure/tfcoil_variables.py
@@ -895,6 +895,12 @@ class TFData:
     drarea: float = 0.0
 
     r_b_tf_inboard_peak_symmetric: float = 0.0
+    
+    ind_tf_self_total: float = 0.0
+    """Sum of all self inductances of the TF coils [H]"""
+    
+    t_tf_charge: float = 0.0
+    """TF coil charging time to operational current [s]"""
 
 
 CREATE_DICTS_FROM_DATACLASS = TFData
