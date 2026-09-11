2f59e9b48de02b87f8d3de13613540bd5f2f99f4
mn3981 <christopher.ashe@ukaea.uk>
2026-09-11 16:45:41 +0100

    Integrate IonTemperatureProfile into PlasmaProfile and update initialization parameters

--- process/models/physics/profiles.py
+++ process/models/physics/profiles.py
@@ -369,7 +369,7 @@ class ElectronDensityProfile(Profile):
                 radius_plasma_pedestal_density_norm=self.data.physics.radius_plasma_pedestal_density_norm,
                 nd_pedestal=self.data.physics.nd_plasma_pedestal_electron,
                 nd_separatrix=self.data.physics.nd_plasma_separatrix_electron,
-                nd_vol_avg=self.data.physics.nd_plasma_electrons_vol_avg,
+                nd_vol_average=self.data.physics.nd_plasma_electrons_vol_avg,
                 alphan=self.data.physics.alphan,
             )
         self.data.physics.nd_plasma_ions_on_axis = (
@@ -662,8 +662,14 @@ class IonTemperatureProfile(Profile):
         """
         self.profile_x = self.electron_temperature_profile.profile_x
         self.profile_dx = self.electron_temperature_profile.profile_dx
+        self.calculate_profile_y()
+        self.integrate_profile_y()
+
+    def calculate_profile_y(self):
+        """Calculate the ion temperature profile based on the electron temperature
+        profile and the ion-to-electron volume-averaged temperature ratio.
+        """
         self.profile_y = (
             self.electron_temperature_profile.profile_y
             * self.data.physics.f_temp_plasma_ion_electron
         )
-        self.integrate_profile_y()
